541d35d417707c99b7e89910674297b33e470865
Sendu Bala <sb10@sanger.ac.uk>
2011-04-01 17:11:49 +0100

    Schema changed to add stacktrace

--- t/VRPipe/Persistent/Schema.pm
+++ t/VRPipe/Persistent/Schema.pm
@@ -2,6 +2,8 @@ use VRPipe::Base;
 
 class t::VRPipe::Persistent::Schema extends DBIx::Class::Schema {
     __PACKAGE__->load_classes({'t::VRPipe' => [qw/Artist CD Track/]});
+    #__PACKAGE__->exception_action(sub { die "moo(@_)\n" });
+    __PACKAGE__->stacktrace(1);
 }
 
 1;
