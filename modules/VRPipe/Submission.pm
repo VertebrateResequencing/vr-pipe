3e6092c918c6e67f1cefc42b3f8fa54b0417b69e
Sendu Bala <sb10@sanger.ac.uk>
2011-04-28 14:44:23 +0100

    Submission now passes through to requirements for various methods, and has extra_* methods

--- modules/VRPipe/Submission.pm
+++ modules/VRPipe/Submission.pm
@@ -26,6 +26,7 @@ class VRPipe::Submission extends VRPipe::Persistent {
                            coerce => 1,
                            required => 1, # even though we're not a key
                            traits => ['VRPipe::Persistent::Attributes'],
+                           # handles => [qw(memory time cpu tmp_space local_space custom)]), *** doesn't work for some reason, and we need them read-only anyway
                            belongs_to => 'VRPipe::Requirements');
     
     has 'scheduler' => (is => 'rw',
@@ -64,6 +65,51 @@ class VRPipe::Submission extends VRPipe::Persistent {
     
     method _build_default_scheduler {
         return VRPipe::Scheduler->get();
+    }
+    
+    method _add_extra (Str $type, Int $extra) {
+        my $new_req = $self->requirements->clone($type => $self->$type() + $extra);
+        $self->requirements($new_req);
+        $self->update;
+    }
+    
+    method memory {
+        return $self->requirements->memory;
+    }
+    method extra_memory (Int $extra = 1000) {
+        $self->_add_extra('memory', $extra);
+    }
+    
+    method time {
+        return $self->requirements->time;
+    }
+    method extra_time (Int $extra = 3600) {
+        $self->_add_extra('time', $extra);
+    }
+    
+    method cpu {
+        return $self->requirements->cpu;
+    }
+    method extra_cpu (Int $extra = 1) {
+        $self->_add_extra('cpu', $extra);
+    }
+    
+    method tmp_space {
+        return $self->requirements->tmp_space;
+    }
+    method extra_tmp_space (Int $extra = 4000) {
+        $self->_add_extra('tmp_space', $extra);
+    }
+    
+    method local_space {
+        return $self->requirements->local_space;
+    }
+    method extra_local_space (Int $extra = 4000) {
+        $self->_add_extra('local_space', $extra);
+    }
+    
+    method custom {
+        return $self->requirements->custom;
     }
     
     __PACKAGE__->make_persistent();
