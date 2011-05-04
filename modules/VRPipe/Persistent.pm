829433ebb8ca9d43e0422279956f382a2271a87b
Sendu Bala <sb10@sanger.ac.uk>
2011-05-04 10:17:33 +0100

    redoing previous fix for creating id-only rows with less code

--- modules/VRPipe/Persistent.pm
+++ modules/VRPipe/Persistent.pm
@@ -354,7 +354,6 @@ class VRPipe::Persistent extends (DBIx::Class::Core, VRPipe::Base::Moose) { # be
                     }
                 }
             }
-            my $just_create = keys %find_args ? 0 : 1;
             
             my $rs = $schema->resultset("$class");
             my $row;
@@ -364,7 +363,7 @@ class VRPipe::Persistent extends (DBIx::Class::Core, VRPipe::Base::Moose) { # be
                     # supplied, but if we supply something that isn't an is_key,
                     # we can generate a new row when we shouldn't do. So we
                     # split up the find and create calls:
-                    my $return = $rs->find(\%find_args) unless $just_create;
+                    my $return = $rs->find(\%find_args) if keys %find_args;
                     
                     if ($return) {
                         # update the row with any non-key args supplied
