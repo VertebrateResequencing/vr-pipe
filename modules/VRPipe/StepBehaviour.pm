455e9b4660e0577568a83ddacce3f12d4c94105f
Sendu Bala <sb10@sanger.ac.uk>
2011-08-09 14:56:18 +0100

    tweaked behaviour methods to accept states, not step numbers

--- modules/VRPipe/StepBehaviour.pm
+++ modules/VRPipe/StepBehaviour.pm
@@ -62,7 +62,7 @@ class VRPipe::StepBehaviour extends VRPipe::Persistent {
             my ($method, @steps) = @$behaviour;
             $self->throw("'$method' is not a valid behaviour") unless $self->can($method);
             
-            return $self->$method(\@steps);
+            return $self->$method($self->step_numbers_to_states(\@steps));
         }
     }
     
@@ -80,11 +80,11 @@ class VRPipe::StepBehaviour extends VRPipe::Persistent {
             }
         }
         
-        return @states;
+        return \@states;
     }
     
-    method delete_outputs (ArrayRef[PositiveInt] $steps) {
-        foreach my $state ($self->step_numbers_to_states($steps)) {
+    method delete_outputs (ArrayRef[VRPipe::StepState] $states) {
+        foreach my $state (@$states) {
             $state->unlink_output_files;
         }
     }
