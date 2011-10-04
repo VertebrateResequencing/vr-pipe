eae2d8b5f297578e1deede014441bddcfad6a420
Sendu Bala <sb10@sanger.ac.uk>
2011-10-04 15:35:12 +0100

    added bam_stats step to the mapping pipelines

--- modules/VRPipe/Pipelines/bam_mapping_with_bwa.pm
+++ modules/VRPipe/Pipelines/bam_mapping_with_bwa.pm
@@ -22,7 +22,7 @@ class VRPipe::Pipelines::bam_mapping_with_bwa with VRPipe::PipelineRole {
                   VRPipe::Step->get(name => 'bwa_sam_using_bam'), #5
                   VRPipe::Step->get(name => 'sam_to_fixed_bam'), #6
                   VRPipe::Step->get(name => 'bam_reheader'), #7
-                  #VRPipe::Step->get(name => 'bam_stats') #8
+                  VRPipe::Step->get(name => 'bam_stats') #8
                  ],
                  
                  [ VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 3, to_key => 'bam_files'),
@@ -32,7 +32,7 @@ class VRPipe::Pipelines::bam_mapping_with_bwa with VRPipe::PipelineRole {
                    VRPipe::StepAdaptorDefiner->new(from_step => 5, to_step => 6, from_key => 'bwa_sam_files', to_key => 'sam_files'),
                    VRPipe::StepAdaptorDefiner->new(from_step => 6, to_step => 7, from_key => 'fixed_bam_files', to_key => 'bam_files'),
                    VRPipe::StepAdaptorDefiner->new(from_step => 1, to_step => 7, from_key => 'reference_dict', to_key => 'dict_file'),
-                   VRPipe::StepAdaptorDefiner->new(from_step => 7, to_step => 8, from_key => 'merged_lane_bams', to_key => 'bam_files') ],
+                   VRPipe::StepAdaptorDefiner->new(from_step => 7, to_step => 8, from_key => 'headed_bam_files', to_key => 'bam_files') ],
                  
                  [ VRPipe::StepBehaviourDefiner->new(after_step => 6, behaviour => 'delete_outputs', act_on_steps => [4, 5], regulated_by => 'cleanup', default_regulation => 1),
                    VRPipe::StepBehaviourDefiner->new(after_step => 7, behaviour => 'delete_outputs', act_on_steps => [6], regulated_by => 'cleanup', default_regulation => 1) ]);
