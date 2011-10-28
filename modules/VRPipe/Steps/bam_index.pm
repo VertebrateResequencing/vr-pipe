c9c230d59ac9ef9ca9e6ba53b3501201ff40067f
Shane McCarthy <sm15@sanger.ac.uk>
2011-10-28 12:00:47 +0100

    check magic of bam index files after we create them

--- modules/VRPipe/Steps/bam_index.pm
+++ modules/VRPipe/Steps/bam_index.pm
@@ -18,11 +18,13 @@ class VRPipe::Steps::bam_index with VRPipe::StepRole {
             my $req = $self->new_requirements(memory => 500, time => 1);
             foreach my $bam (@{$self->inputs->{bam_files}}) {
                 my $bam_path = $bam->path;
-                my $bai_path = $self->output_file(output_key => 'bai_files',
+                my $bai_file = $self->output_file(output_key => 'bai_files',
                                               output_dir => $bam->dir,
                                               basename => $bam->basename.'.bai',
-                                              type => 'bin')->path;
-                $self->dispatch([qq[$samtools index $bam_path $bai_path], $req]);
+                                              type => 'bin');
+                my $bai_path = $bai_file->path;
+                my $cmd = qq[$samtools index $bam_path $bai_path];
+                $self->dispatch_wrapped_cmd('VRPipe::Steps::bam_index', 'index_and_check', [$cmd, $req, {output_files => [$bai_file]}]); 
             }
         };
     }
@@ -37,6 +39,29 @@ class VRPipe::Steps::bam_index with VRPipe::StepRole {
     }
     method max_simultaneous {
         return 0; # meaning unlimited
+    }
+    method index_and_check (ClassName|Object $self: Str $cmd_line) {
+        my ($bam_path, $bai_path) = $cmd_line =~ /index (\S+) (\S+)/;
+        $bam_path || $self->throw("cmd_line [$cmd_line] was not constructed as expected");
+        $bai_path || $self->throw("cmd_line [$cmd_line] was not constructed as expected");
+        
+        my $bam_file = VRPipe::File->get(path => $bam_path);
+        my $bai_file = VRPipe::File->get(path => $bai_path);
+        
+        $bam_file->disconnect;
+        system($cmd_line) && $self->throw("failed to run [$cmd_line]");
+        
+        $bai_file->update_stats_from_disc(retries => 3);
+        
+        my $correct_magic = [qw(102 101 111 001)];
+        
+        if ($bai_file->check_magic($bai_file->path->absolute->stringify, $correct_magic)) {
+            return 1;
+        }
+        else {
+            $bai_file->unlink;
+            $self->throw("cmd [$cmd_line] failed because index file $bai_path had the incorrect magic");
+        }
     }
 }
 
