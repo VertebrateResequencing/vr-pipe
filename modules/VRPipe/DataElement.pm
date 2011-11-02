69c2aac843fa32a1d3d227e3a4e6e36b3280befa
Sendu Bala <sb10@sanger.ac.uk>
2011-11-02 16:06:54 +0000

    DataElement has new element_states method; removed changed bool column and replaced with _changed_marker

--- modules/VRPipe/DataElement.pm
+++ modules/VRPipe/DataElement.pm
@@ -18,12 +18,22 @@ class VRPipe::DataElement extends VRPipe::Persistent {
                         traits => ['VRPipe::Persistent::Attributes'],
                         default => 0);
     
-    has 'changed' => (is => 'rw',
-                      isa => 'Bool',
-                      traits => ['VRPipe::Persistent::Attributes'],
-                      default => 0);
+    has '_changed_marker' => (is => 'rw',
+                             isa => Varchar[255],
+                             traits => ['VRPipe::Persistent::Attributes'],
+                             is_nullable => 1);
     
-    __PACKAGE__->make_persistent();
+    __PACKAGE__->make_persistent(); # has_many => [element_states => 'VRPipe::DataElementState'] doesn't work because of ordering issues?
+    
+    method element_states {
+        my $schema = $self->result_source->schema;
+        my $rs = $schema->resultset('DataElementState')->search({ dataelement => $self });
+        my @states;
+        while (my $state = $rs->next) {
+            push(@states, $state);
+        }
+        return @states;
+    }
     
     method start_from_scratch (VRPipe::PipelineSetup $setup, ArrayRef[PositiveInt] $step_numbers?) {
         VRPipe::DataElementState->get(pipelinesetup => $setup, dataelement => $self)->start_from_scratch($step_numbers ? $step_numbers : ());
