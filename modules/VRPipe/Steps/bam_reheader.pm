0091c705a023a487a9a85caba08d6a5d6df20c1e
Shane McCarthy <sm15@sanger.ac.uk>
2011-11-22 09:58:48 +0000

    mergeup pipeline and test

--- modules/VRPipe/Steps/bam_reheader.pm
+++ modules/VRPipe/Steps/bam_reheader.pm
@@ -75,7 +75,8 @@ class VRPipe::Steps::bam_reheader with VRPipe::StepRole {
                                                                        bases => 'total number of base pairs',
                                                                        reads => 'total number of reads (sequences)',
                                                                        paired => '0=unpaired reads were mapped; 1=paired reads were mapped; 2=mixture of paired and unpaired reads were mapped',
-                                                                       optional => ['library', 'insert_size', 'sample', 'center_name', 'platform', 'study']}) };
+                                                                       merged_bams => 'comma separated list of merged bam paths',
+                                                                       optional => ['library', 'insert_size', 'sample', 'center_name', 'platform', 'study', 'merged_bams']}) };
     }
     method post_process_sub {
         return sub { return 1; };
@@ -106,33 +107,54 @@ class VRPipe::Steps::bam_reheader with VRPipe::StepRole {
         }
         $dict_file->close;
         
-        # construct the RG line from the bam metadata
+        # construct the RG lines from the bam metadata
+        # bam may be a merge of mupltiple bams, so we construct the RG lines
+        # from the metadata of each of the merged bams
         my $headed_bam_file = VRPipe::File->get(path => $output);
         my $meta = $headed_bam_file->metadata;
-        print $hfh "\@RG\tID:", $meta->{lane};
-        if (defined $meta->{library}) {
-            print $hfh "\tLB:", $meta->{library};
+        my @files;
+        if (defined $meta->{merged_bams}) {
+            my @paths = split /,/, $meta->{merged_bams};
+            foreach my $path (@paths) {
+                push @files, VRPipe::File->get(path => $path);
+            }
+        } else {
+            push @files, $headed_bam_file;
         }
-        if (defined $meta->{sample}) {
-            print $hfh "\tSM:", $meta->{sample};
-        }
-        if (defined $meta->{insert_size}) {
-            print $hfh "\tPI:", $meta->{insert_size};
-        }
-        if (defined $meta->{center_name}) {
-            print $hfh "\tCN:", $meta->{center_name};
-        }
-        if (defined $meta->{platform}) {
-            print $hfh "\tPL:", $meta->{platform};
-        }
-        if (defined $meta->{study}) {
-            print $hfh "\tDS:", $meta->{study};
-        }
-        print $hfh "\n";
-        $header_lines++;
         
-        # construct a chain of PG lines for the header by looking at previous
-        # steps in our pipeline
+        my %lanes;
+        foreach my $file (@files) {
+            my $this_meta = $file->metadata;
+            # we may have merged multiple bams from the same lane - eg single and paired 
+            # end bams, so only print readgroup once
+            next if (exists $lanes{$this_meta->{lane}});
+            $lanes{$this_meta->{lane}} = 1;
+            print $hfh "\@RG\tID:", $this_meta->{lane};
+            if (defined $this_meta->{library}) {
+                print $hfh "\tLB:", $this_meta->{library};
+            }
+            if (defined $this_meta->{sample}) {
+                print $hfh "\tSM:", $this_meta->{sample};
+            }
+            if (defined $this_meta->{insert_size}) {
+                print $hfh "\tPI:", $this_meta->{insert_size};
+            }
+            if (defined $this_meta->{center_name}) {
+                print $hfh "\tCN:", $this_meta->{center_name};
+            }
+            if (defined $this_meta->{platform}) {
+                print $hfh "\tPL:", $this_meta->{platform};
+            }
+            if (defined $this_meta->{study}) {
+                print $hfh "\tDS:", $this_meta->{study};
+            }
+            print $hfh "\n";
+            $header_lines++;
+        }
+        
+        # Construct a chain of PG lines for the header by looking at previous
+        # steps in our pipeline. If this the bam was produced from a vrpipe 
+        # datasource, find it's parent elements and include in the program chain
         my $this_step_state = VRPipe::StepState->get(id => $step_state);
         my $pipelinesetup = $this_step_state->pipelinesetup;
         my $dataelement = $this_step_state->dataelement;
@@ -140,20 +162,20 @@ class VRPipe::Steps::bam_reheader with VRPipe::StepRole {
         my $this_stepm_id = $stepmember->id;
         my $pipeline = $stepmember->pipeline;
         my $pp;
-        foreach my $stepm ($pipeline->step_members) {
-            last if $stepm->id == $this_stepm_id;
-            
-            my $cmd_summary = VRPipe::StepState->get(pipelinesetup => $pipelinesetup, stepmember => $stepm, dataelement => $dataelement)->cmd_summary || next;
-            
-            my $step_name = $stepm->step->name;
-            print $hfh "\@PG\tID:$step_name\tPN:", $cmd_summary->exe, "\t";
+        my %dups;
+        foreach my $pg_line ($self->command_history(pipelinesetup => $pipelinesetup, stepmember => $stepmember, dataelement => $dataelement))
+        {
+            my $uniq = join '::', ($pg_line->{program_id}, $pg_line->{program_name}, $pg_line->{program_version});
+            $uniq =~ s/\W/_/g;
+            next if (exists $dups{$uniq});
+            $dups{$uniq} = 1;
+            print $hfh "\@PG\tID:", $pg_line->{program_id}, "\tPN:", $pg_line->{program_name}, "\t";
             if ($pp) {
-                print $hfh "PP:$pp\t";
+                print $hfh "PP:", $pp, "\t";
             }
-            print $hfh "VN:", $cmd_summary->version, "\tCL:", $cmd_summary->summary, "\n";
+            print $hfh "VN:", $pg_line->{program_version}, "\tCL:", $pg_line->{command_line}, "\n";
             $header_lines++;
-            
-            $pp = $step_name;
+            $pp = $pg_line->{program_id};
         }
         if ($comment) {
             my $comment_file = VRPipe::File->get(path => $comment);
@@ -182,6 +204,32 @@ class VRPipe::Steps::bam_reheader with VRPipe::StepRole {
             $headed_bam_file->unlink;
             $self->throw("cmd [$cmd_line] failed because $actual_lines lines were generated in the reheaded bam file, yet there were $expected_lines records in the input bam files and header");
         }
+    }
+    method command_history (ClassName|Object $self: VRPipe::PipelineSetup :$pipelinesetup, VRPipe::DataElement :$dataelement, VRPipe::StepMember :$stepmember) {
+        my $this_stepm_id = $stepmember->id;
+        my $pipeline = $stepmember->pipeline;
+        my $m = VRPipe::Manager->get;
+        my $schema = $m->result_source->schema;
+        my @program_chain;
+        if ($pipelinesetup->datasource->type eq 'vrpipe') {
+            my $vrpipe_sources = $pipelinesetup->datasource->_source_instance->vrpipe_sources;
+            foreach my $setup_id (keys %{$vrpipe_sources}) {
+                my $this_pipelinesetup = VRPipe::PipelineSetup->get(id => $setup_id);
+                my $this_stepmember = VRPipe::StepMember->get(id => $vrpipe_sources->{$setup_id}->{final_smid});
+                my $rs = $schema->resultset('DataElementLink')->search({ pipelinesetup => $setup_id, child => $dataelement->id });
+                while (my $link = $rs->next) {
+                    push @program_chain, $self->command_history(pipelinesetup => $this_pipelinesetup, dataelement => $link->parent, stepmember => $this_stepmember);
+                }
+            }
+        }
+        foreach my $stepm ($pipeline->step_members) {
+            my $cmd_summary = VRPipe::StepState->get(pipelinesetup => $pipelinesetup, stepmember => $stepm, dataelement => $dataelement)->cmd_summary || next;
+            my $step_name = $stepm->step->name;
+            my $pg_hash = { program_id => $step_name, program_name => $cmd_summary->exe, program_version => $cmd_summary->version, command_line => $cmd_summary->summary };
+            push @program_chain, $pg_hash;
+            last if $stepm->id == $this_stepm_id;
+        }
+        return @program_chain;
     }
 }
 
