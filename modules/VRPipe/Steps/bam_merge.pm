320a826c1b5fa3449fd776ff5898597f11e45fab
Sendu Bala <sb10@sanger.ac.uk>
2011-12-12 13:52:50 +0000

    use normal symlinking when the symlink file is supposed to have different metadata to the source

--- modules/VRPipe/Steps/bam_merge.pm
+++ modules/VRPipe/Steps/bam_merge.pm
@@ -123,7 +123,7 @@ class VRPipe::Steps::bam_merge extends VRPipe::Steps::picard {
         }
         
         if (scalar @in_paths == 1) {
-            $in_files[0]->symlink($out_file);
+            symlink($in_files[0]->path, $out_file->path); # don't use $in_files[0]->symlink because $out_file->metadata will be different
         } else {
             $out_file->disconnect;
             system($cmd_line) && $self->throw("failed to run [$cmd_line]");
