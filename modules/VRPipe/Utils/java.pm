f16eb9668febd1c03161d7f675e0b116427f5e2e
Sendu Bala <sb10@sanger.ac.uk>
2012-01-04 13:49:42 +0000

    Potential fix for bam_mark_duplicates constantly failing
    This step fails repeatidly due to a random assortment of java
    startup memory problems, probably when multiple of these try
    to run on the same node.
    
    Picard now has more overhead between reserved memory by the
    scheduler and java -Xmx (0.7 instead of 0.9), and the
    bam_mark_duplicates step in particular had its memory reservation
    bumped to 5.8GB. This keeps the actual desired memory the same
    at around 4GB, and should also mean the scheduler is less
    likely to schedule multiple of these jobs on the same node.

--- modules/VRPipe/Utils/java.pm
+++ modules/VRPipe/Utils/java.pm
@@ -1,13 +1,16 @@
 use VRPipe::Base;
 
 class VRPipe::Utils::java {
-    
     has 'java_exe' => (is => 'ro',
                        isa => 'Str',
                        default => 'java');
     
+    has 'memory_multiplier' => (is => 'ro',
+                                isa => 'Num',
+                                default => 0.9);
+    
     method jvm_args (ClassName|Object $self: Int $memory, Str|Dir $dir?) {
-        my $java_mem = int($memory * 0.9);
+        my $java_mem = int($memory * $self->memory_multiplier);
         my $xss = 280;
         if ($java_mem > 1000)
         {
