9638760fa82436482c13a8226fb917d3e4d8c988
John Maslen <jm23@sanger.ac.uk>
2012-01-25 14:51:38 +0000

    tweaked to inherit options from  mpileup_bcf

--- modules/VRPipe/Steps/mpileup_vcf.pm
+++ modules/VRPipe/Steps/mpileup_vcf.pm
@@ -1,37 +1,15 @@
 use VRPipe::Base;
 
-class VRPipe::Steps::mpileup_vcf with VRPipe::StepRole {
-	method options_definition {
-		return { samtools_exe => VRPipe::StepOption->get(description => 'path to samtools executable',
-				optional => 1,
-				default_value => 'samtools'),
-			   samtools_mpileup_options => VRPipe::StepOption->get(description => 'samtools mpileup options excluding -f',
-					   optional => 1,
-					   default_value => '-C50 -aug'),
-			   reference_fasta => VRPipe::StepOption->get(description => 'absolute path to reference genome fasta'), 
-			   bcftools_exe => VRPipe::StepOption->get(description => 'path to bcftools executable',
-					   optional => 1,
-					   default_value => 'bcftools'),
-			   bcftools_view_options => VRPipe::StepOption->get(description => 'bcftools view options',
-					   optional => 1,
-					   default_value => '-gcv'),
-			   max_cmdline_bams => VRPipe::StepOption->get(description => 'max number of bam filenames to allow on command line', 
-					   optional => 1, 
-					   default_value => 10),
-			   interval_list => VRPipe::StepOption->get(description => 'absolute path to targets interval list file for -l option', 
-					   optional => 1,),
-		};
-	}
+class VRPipe::Steps::mpileup_vcf extends VRPipe::Steps::mpileup_bcf {
     method inputs_definition {
         return { bam_files => VRPipe::StepIODefinition->get(type => 'bam', max_files => -1, description => '1 or more bam files to call variants'),
 		chunked_regions_file => VRPipe::StepIODefinition->get(type => 'txt', min_files => 0, max_files => 1, description => 'file of chomosome region chunks to run concurrently'),
 		 };
     }
-
     method body_sub {
         return sub {
-
             my $self = shift;
+            use VRPipe::Steps::mpileup_bcf;
             my $options = $self->options;
             my $samtools = $options->{samtools_exe};
             my $reference_fasta = $options->{reference_fasta};
@@ -82,14 +60,8 @@ class VRPipe::Steps::mpileup_vcf with VRPipe::StepRole {
     method outputs_definition {
         return { vcf_files => VRPipe::StepIODefinition->get(type => 'bin', max_files => -1, description => 'either a single .vcf.gz file, or a chunk set of vcf.gz files, for each set of input bam files') };
     }
-    method post_process_sub {
-        return sub { return 1; };
-    }
     method description {
         return "Run samtools mpileup for one or more bams, generating either one vcf per set of bams, or a chunk set per bam set if chunked_regions_file is provided";
-    }
-    method max_simultaneous {
-        return 0; # meaning unlimited
     }
 }
 
