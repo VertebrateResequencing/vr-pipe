469e36258342c7e1efe1400d30fcd9b1b6330bc4
Sendu Bala <sb10@sanger.ac.uk>
2012-03-07 15:48:08 +0000

    -p and -r are no longer allowed as options; output files are now defined, and they have caption metadata

--- modules/VRPipe/Steps/plot_bamcheck.pm
+++ modules/VRPipe/Steps/plot_bamcheck.pm
@@ -6,9 +6,8 @@ class VRPipe::Steps::plot_bamcheck with VRPipe::StepRole {
         return { plot_bamcheck_exe => VRPipe::StepOption->get(description => 'path to plot-bamcheck executable',
                                                          optional => 1,
                                                          default_value => 'plot-bamcheck'),
-                 plot_bamcheck_options => VRPipe::StepOption->get(description => 'options for plot-bamcheck',
-                                                         optional => 1,
-                                                         default_value => '-p'),
+                 plot_bamcheck_options => VRPipe::StepOption->get(description => 'options for plot-bamcheck, excluding -p and -r',
+                                                         optional => 1),
                  reference_fasta_stats => VRPipe::StepOption->get(description => 'absolute path to genome reference stats file used in the plotting process', optional => 1),
                };
     }
@@ -21,27 +20,62 @@ class VRPipe::Steps::plot_bamcheck with VRPipe::StepRole {
         return sub {
             my $self = shift;
             my $options = $self->options;
-            my $stats = '';
-            if ($options->{reference_fasta_stats}) {
-            	$stats = Path::Class::File->new($options->{reference_fasta_stats});
-            	$self->throw("reference_fasta_stats must be an absolute path") unless $stats->is_absolute;
-            }
             my $req = $self->new_requirements(memory => 500, time => 1);
             my $plot_bc_exe = $options->{plot_bamcheck_exe};
             my $plot_bc_opts = $options->{plot_bamcheck_options};
+	    if ($plot_bc_opts =~ /-p|-r/) {
+		$self->throw("plot_bamcheck_options must not contain -p or -r");
+	    }
+            if ($options->{reference_fasta_stats}) {
+            	my $ref_stats = Path::Class::File->new($options->{reference_fasta_stats});
+            	$self->throw("reference_fasta_stats must be an absolute path") unless $ref_stats->is_absolute;
+		$plot_bc_opts .= '-r '.$ref_stats;
+            }
+	    
             foreach my $bc_file (@{$self->inputs->{bamcheck_files}}) {
                 my $meta = $bc_file->metadata;
-                my $sample = $meta->{sample} ? $meta->{sample} : '';
-                my $bc_path = $bc_file->path;
-                my $graph_dir = $bc_path->dir."/$sample";
-                my $cmd = qq[$plot_bc_exe $plot_bc_opts $graph_dir $stats $bc_path];
+		my $prefix = $meta->{lane} || $bc_file->basename;
+		my $source_bam = $meta->{source_bam};
+		
+		$self->output_file(temporary => 1, basename => $prefix.'-quals.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-quals.png', type => 'bin', metadata => {caption => 'Qualities', source_bam => $source_bam});
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-quals2.png', type => 'bin', metadata => {caption => 'Qualities', source_bam => $source_bam});
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-quals3.png', type => 'bin', metadata => {caption => 'Qualities', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-quals-hm.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-quals-hm.png', type => 'bin', metadata => {caption => 'Qualities', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-insert-size.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-insert-size.png', type => 'bin', metadata => {caption => 'Insert Size', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-gc-content.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-gc-content.png', type => 'bin', metadata => {caption => 'GC Content', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-gc-depth.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-gc-depth.png', type => 'bin', metadata => {caption => 'GC Depth', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-acgt-cycles.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-acgt-cycles.png', type => 'bin', metadata => {caption => 'ACGT Cycles', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-coverage.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-coverage.png', type => 'bin', metadata => {caption => 'Coverage', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-indel-dist.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-indel-dist.png', type => 'bin', metadata => {caption => 'Indel distribution', source_bam => $source_bam});
+		$self->output_file(temporary => 1, basename => $prefix.'-indel-cycles.gp', type => 'txt');
+		$self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-indel-cycles.png', type => 'bin', metadata => {caption => 'Indels per cycle', source_bam => $source_bam});
+		
+		#*** really we need to know if the bamcheck file was produced using 'bamcheck -r'
+		if ($options->{reference_fasta_stats}) {
+		    $self->output_file(temporary => 1, basename => $prefix.'-mism-per-cycle.gp', type => 'txt');
+		    $self->output_file(output_key => 'bamcheck_plots', basename => $prefix.'-mism-per-cycle.png', type => 'bin', metadata => {caption => 'Mismatches per cycle', source_bam => $source_bam});
+		}
+		
+		my $bc_path = $bc_file->path;
+                my $cmd = qq[$plot_bc_exe $plot_bc_opts -p $prefix $bc_path];
                 $self->dispatch([$cmd, $req]);
             }
         };
     }
 
     method outputs_definition {
-        return { };
+        return { bamcheck_plots => VRPipe::StepIODefinition->get(type => 'bin',
+                                                                 description => 'png files produced by plot-bamcheck, with a caption in the metadata',
+								 min_files => 11,
+                                                                 max_files => -1) };
     }
     
     method post_process_sub {
