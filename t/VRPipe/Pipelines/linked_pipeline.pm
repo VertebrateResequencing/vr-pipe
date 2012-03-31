bb50dbbf27892ccee8ff3e5b8c755d2b316145f4
Sendu Bala <sb10@sanger.ac.uk>
2012-03-31 22:50:43 +0100

    vrpipe datasource now waits for parent element to complete its pipeline, preventing issues when the child pipeline deletes the parents files while it still needs them

--- t/VRPipe/Pipelines/linked_pipeline.pm
+++ t/VRPipe/Pipelines/linked_pipeline.pm
@@ -19,7 +19,8 @@ class VRPipe::Pipelines::linked_pipeline with VRPipe::PipelineRole {
                   VRPipe::Step->get(name => 'test_step_one') ],
                 [ VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 1, to_key => 'input_text_files'),
                   VRPipe::StepAdaptorDefiner->new(from_step => 1, to_step => 2, from_key => 'merged_file', to_key => 'one_input') ],
-                [ VRPipe::StepBehaviourDefiner->new(after_step => 2, behaviour => 'delete_outputs', act_on_steps => [1], regulated_by => 'cleanup', default_regulation => 1) ]);
+                [ VRPipe::StepBehaviourDefiner->new(after_step => 2, behaviour => 'delete_outputs', act_on_steps => [1], regulated_by => 'cleanup', default_regulation => 1),
+                  VRPipe::StepBehaviourDefiner->new(after_step => 1, behaviour => 'delete_inputs', act_on_steps => [0], regulated_by => 'delete_inputs', default_regulation => 0) ]);
     }
 }
 
