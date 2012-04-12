6ffeec95d382987b3f5fbcb4a4cd87edda7b89c6
Sendu Bala <sb10@sanger.ac.uk>
2012-04-12 16:31:19 +0100

    gtypex_genotype_analysis no longer outputs a gtype file; instead it adds metadata to the input bam file

--- modules/VRPipe/Steps/gtypex_genotype_analysis.pm
+++ modules/VRPipe/Steps/gtypex_genotype_analysis.pm
@@ -22,42 +22,32 @@ class VRPipe::Steps::gtypex_genotype_analysis with VRPipe::StepRole {
 		my $meta = $gtypex->metadata;
 		my $expected = $meta->{sample};
 		my $source_bam = $meta->{source_bam};
-            	my $bam_basename = file($source_bam)->basename;
-                my $gtypex_path = $gtypex->path;          
-                my $gtype_file = $self->output_file(output_key => 'gtype_files',
-                                                    basename => $bam_basename.'.gtype',
-                                                    type => 'txt',
-                                                    metadata => {sample => $expected, source_bam => $source_bam});
-                my $gtype_path = $gtype_file->path;
-                my $cmd = "use VRPipe::Steps::gtypex_genotype_analysis; VRPipe::Steps::gtypex_genotype_analysis->analyse_gtypex_output(gtypex => q[$gtypex_path], gtype => q[$gtype_path], confidence => q[$min_ratio]);";
-                $self->dispatch_vrpipecode($cmd, $req, {output_files => [$gtype_file]});
+                my $gtypex_path = $gtypex->path;
+                my $cmd = "use VRPipe::Steps::gtypex_genotype_analysis; VRPipe::Steps::gtypex_genotype_analysis->analyse_gtypex_output(gtypex => q[$gtypex_path], confidence => q[$min_ratio], source_bam => q[$source_bam]);";
+                $self->dispatch_vrpipecode($cmd, $req);
             }
         };
     }
     method outputs_definition {
-        return { gtype_files => VRPipe::StepIODefinition->get(type => 'txt',
-							      max_files => -1,
-							      description => 'file of genotype status report for the sample',
-							      metadata => {sample => 'sample name'}) };
+        return { };
     }
     method post_process_sub {
         return sub { return 1; }
     }
     method description {
-        return "The genotype status is determined by analysing the gtypex file of likelihood scores of sample genotypes and comparing the score ratio (over the 'next best' genotype) with the confidence level cutoff provided";
+        return "The genotype status is determined by analysing the gtypex file of likelihood scores of sample genotypes and comparing the score ratio (over the 'next best' genotype) with the confidence level cutoff provided; results are stored as metadata (gtype_analysis) on the bam file";
     }
     method max_simultaneous {
         return 0;
     }
     
-    method analyse_gtypex_output (ClassName|Object $self: Str|File :$gtypex, Str|File :$gtype, Num :$confidence) {
+    method analyse_gtypex_output (ClassName|Object $self: Str|File :$gtypex, Str|File :$source_bam, Num :$confidence) {
         my $gtypex_file = VRPipe::File->get(path => $gtypex);
         my $meta = $gtypex_file->metadata;
         my $expected = $meta->{sample};
-        my $gtype_file = VRPipe::File->get(path => $gtype);
+        my $bam_file = VRPipe::File->get(path => $source_bam);
         my $fh = $gtypex_file->openr;
-        my $ofh = $gtype_file->openw;
-        $gtype_file->disconnect;
+        $bam_file->disconnect;
         my $has_data = 0;
     	my ($hit1,$hit2,$gtype1,$lhood1,$gtype2,$lhood2);
 	my $entrp = <$fh>;
@@ -83,29 +73,32 @@ class VRPipe::Steps::gtypex_genotype_analysis with VRPipe::StepRole {
         my $expected_gtype2 = ($expected eq $gtype2 && $lhood1 == $lhood2) ? 1 : 0;
         if ( $expected && !$has_data ) { $expected = 0; }
         my $ratio = $lhood1!=0 ? $lhood2/$lhood1 : $lhood2/1e-6;
+	$ratio = sprintf("%0.3f", $ratio);
         my $gt_status;
         if ( $expected_gtype2 ) {
-            $gt_status = "status=confirmed expected=$expected found=$gtype2 ratio=$ratio\n"; 
+            $gt_status = "status=confirmed expected=$expected found=$gtype2 ratio=$ratio"; 
         }
         elsif ( $ratio<$confidence ) {
             if ( $expected ) {
-                $gt_status = "status=unconfirmed expected=$expected found=$gtype1 ratio=$ratio\n"; 
+                $gt_status = "status=unconfirmed expected=$expected found=$gtype1 ratio=$ratio"; 
             }
             else {
-               	$gt_status = "status=unknown expected=none found=$gtype1 ratio=$ratio\n";
+               	$gt_status = "status=unknown expected=none found=$gtype1 ratio=$ratio";
             }	
         }
         elsif ( !$expected ) {
-            $gt_status = "status=candidate expected=none found=$gtype1 ratio=$ratio\n"; 
+            $gt_status = "status=candidate expected=none found=$gtype1 ratio=$ratio"; 
         }
         elsif ( $expected eq $gtype1 ) {
-            $gt_status = "status=confirmed expected=$expected found=$gtype1 ratio=$ratio\n"; 
+            $gt_status = "status=confirmed expected=$expected found=$gtype1 ratio=$ratio"; 
         }
         else {
-            $gt_status = "status=wrong expected=$expected found=$gtype1 ratio=$ratio\n";
+            $gt_status = "status=wrong expected=$expected found=$gtype1 ratio=$ratio";
         }
-        print $ofh $gt_status;
-        close $ofh;
+        
+	my $new_meta = {gtype_analysis => $gt_status};
+	$bam_file->add_metadata($new_meta, replace_data => 1);
+	$gtypex_file->add_metadata($new_meta, replace_data => 1);
     }
 }
 
