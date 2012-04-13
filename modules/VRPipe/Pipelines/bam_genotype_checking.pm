5d84eb8deb3e8fba54650d376d0b5353c002dd52
Sendu Bala <sb10@sanger.ac.uk>
2012-04-13 13:59:30 +0100

    removed extraneous step adaptor, which prevented pipeline-setup from working with this pipeline

--- modules/VRPipe/Pipelines/bam_genotype_checking.pm
+++ modules/VRPipe/Pipelines/bam_genotype_checking.pm
@@ -21,8 +21,7 @@ class VRPipe::Pipelines::bam_genotype_checking with VRPipe::PipelineRole {
                   VRPipe::Step->get(name => 'gtypex_genotype_analysis'),#4
                   ],
                  
-                 [ VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 1, to_key => 'hapmap_file'),
-                   VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 2, to_key => 'bam_files'),
+                 [ VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 2, to_key => 'bam_files'),
                    VRPipe::StepAdaptorDefiner->new(from_step => 1, to_step => 2, from_key => 'hapmap_file', to_key => 'hapmap_file'),
                    VRPipe::StepAdaptorDefiner->new(from_step => 2, to_step => 3, from_key => 'bcf_files_with_metadata', to_key => 'bcf_files'),
                    VRPipe::StepAdaptorDefiner->new(from_step => 3, to_step => 4, from_key => 'gtypex_files_with_metadata', to_key => 'gtypex_files'),
