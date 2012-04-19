8885652113525fdae177e241f0a0210cf1de64c8
Sendu Bala <sb10@sanger.ac.uk>
2012-04-19 16:35:57 +0100

    updated to use new VRTrack->transaction() method, which should stop constant failues due to deadlocking

--- modules/VRPipe/Steps/vrtrack_update_mapstats.pm
+++ modules/VRPipe/Steps/vrtrack_update_mapstats.pm
@@ -63,70 +63,75 @@ class VRPipe::Steps::vrtrack_update_mapstats extends VRPipe::Steps::vrtrack_upda
 	my $vrtrack = $self->get_vrtrack(db => $db);
 	my $vrlane = VRTrack::Lane->new_by_hierarchy_name($vrtrack, $lane) || $self->throw("No lane named '$lane' in database '$db'");
 	my $mapstats = $vrlane->latest_mapping;
-	$vrtrack->transaction_start();
-	unless ($mapstats) {
-	    $mapstats = $vrlane->add_mapping();
-	    #*** do we need to fill in mapper and assembly?
-	}
 	
-	# fill in the mapstats based on our bam metadata
-	my $meta_key_prefix = $targets_mode ? 'targeted_' : '';
-	
-	$mapstats->raw_reads($meta->{$meta_key_prefix.'reads'});
-	my $raw_bases = $meta->{$meta_key_prefix.'bases'};
-	$mapstats->raw_bases($raw_bases);
-	$mapstats->reads_mapped($meta->{$meta_key_prefix.'reads_mapped'});
-	$mapstats->reads_paired($meta->{$meta_key_prefix.'reads_paired'});
-	$mapstats->bases_mapped($meta->{$meta_key_prefix.'bases_mapped_c'});
-	$mapstats->error_rate($meta->{$meta_key_prefix.'error_rate'});
-	$mapstats->rmdup_reads_mapped($meta->{$meta_key_prefix.'rmdup_reads_mapped'});
-	$mapstats->rmdup_bases_mapped($meta->{$meta_key_prefix.'rmdup_bases_mapped'}); # ideally this would be rmdup_bases_mapped_c, but we no longer calculate this with bamcheck -d
-	$mapstats->clip_bases($raw_bases - $meta->{$meta_key_prefix.'bases_trimmed'});
-	$mapstats->mean_insert($meta->{$meta_key_prefix.'mean_insert_size'});
-	$mapstats->sd_insert($meta->{$meta_key_prefix.'sd_insert_size'});
-	
-	if ($targets_mode) {
-	    foreach my $cov (1, 2, 5, 10, 20, 50, 100) {
-		my $these_bases = $meta->{"targeted_bases_of_${cov}X_coverage"} || next;
-		my $method = "target_bases_${cov}X";
-		$mapstats->$method(sprintf("%0.2f", $these_bases / $raw_bases));
+	my $worked = $vrtrack->transaction(sub {
+	    unless ($mapstats) {
+		$mapstats = $vrlane->add_mapping();
+		#*** do we need to fill in mapper and assembly?
 	    }
-	    $mapstats->mean_target_coverage($meta->{targeted_mean_coverage}) if $meta->{targeted_mean_coverage};
-	    $mapstats->target_bases_mapped($meta->{targeted_bases_mapped_c});
+	    
+	    # fill in the mapstats based on our bam metadata
+	    my $meta_key_prefix = $targets_mode ? 'targeted_' : '';
+	    
+	    $mapstats->raw_reads($meta->{$meta_key_prefix.'reads'});
+	    my $raw_bases = $meta->{$meta_key_prefix.'bases'};
+	    $mapstats->raw_bases($raw_bases);
+	    $mapstats->reads_mapped($meta->{$meta_key_prefix.'reads_mapped'});
+	    $mapstats->reads_paired($meta->{$meta_key_prefix.'reads_paired'});
+	    $mapstats->bases_mapped($meta->{$meta_key_prefix.'bases_mapped_c'});
+	    $mapstats->error_rate($meta->{$meta_key_prefix.'error_rate'});
+	    $mapstats->rmdup_reads_mapped($meta->{$meta_key_prefix.'rmdup_reads_mapped'});
+	    $mapstats->rmdup_bases_mapped($meta->{$meta_key_prefix.'rmdup_bases_mapped'}); # ideally this would be rmdup_bases_mapped_c, but we no longer calculate this with bamcheck -d
+	    $mapstats->clip_bases($raw_bases - $meta->{$meta_key_prefix.'bases_trimmed'});
+	    $mapstats->mean_insert($meta->{$meta_key_prefix.'mean_insert_size'});
+	    $mapstats->sd_insert($meta->{$meta_key_prefix.'sd_insert_size'});
+	    
+	    if ($targets_mode) {
+		foreach my $cov (1, 2, 5, 10, 20, 50, 100) {
+		    my $these_bases = $meta->{"targeted_bases_of_${cov}X_coverage"} || next;
+		    my $method = "target_bases_${cov}X";
+		    $mapstats->$method(sprintf("%0.2f", $these_bases / $raw_bases));
+		}
+		$mapstats->mean_target_coverage($meta->{targeted_mean_coverage}) if $meta->{targeted_mean_coverage};
+		$mapstats->target_bases_mapped($meta->{targeted_bases_mapped_c});
+	    }
+	    
+	    # add the images
+	    while (my ($path, $caption) = each %plot_files) {
+		my $img = $mapstats->add_image_by_filename($path);
+		$img->caption($caption);
+		$img->update;
+	    }
+	    
+	    $mapstats->update;
+	    
+	    # say that the file is imported
+	    my $vrfile = $vrlane->get_file_by_name($bam_file->basename);
+	    if ($vrfile) {
+		$vrfile->is_processed(import => 1);
+		$vrfile->md5($bam_file->md5);
+		$vrfile->update;
+		$vrfile->is_processed(mapped => 1);
+		$vrfile->update;
+	    }
+	    
+	    # also update the lane
+	    $vrlane->raw_bases($meta->{bases});
+	    $vrlane->raw_reads($meta->{reads});
+	    $vrlane->is_paired($meta->{paired} ? 1 : 0);
+	    $vrlane->read_len(int($meta->{avg_read_length}));
+	    $vrlane->qc_status('pending');
+	    $vrlane->is_processed(import => 1);
+	    $vrlane->update;
+	    $vrlane->is_processed(mapped => 1);
+	    $vrlane->update;
+	    $vrlane->is_processed(qc => 1);
+	    $vrlane->update;
+	});
+	
+	unless ($worked) {
+	    $self->throw($vrtrack->{transaction_error});
 	}
-	
-	# add the images
-	while (my ($path, $caption) = each %plot_files) {
-	    my $img = $mapstats->add_image_by_filename($path);
-	    $img->caption($caption);
-	    $img->update;
-	}
-	
-	$mapstats->update;
-	
-	# say that the file is imported
-	my $vrfile = $vrlane->get_file_by_name($bam_file->basename);
-	if ($vrfile) {
-	    $vrfile->is_processed(import => 1);
-	    $vrfile->md5($bam_file->md5);
-	    $vrfile->update;
-	    $vrfile->is_processed(mapped => 1);
-	    $vrfile->update;
-	}
-	
-	# also update the lane
-	$vrlane->raw_bases($meta->{bases});
-	$vrlane->raw_reads($meta->{reads});
-	$vrlane->is_paired($meta->{paired} ? 1 : 0);
-	$vrlane->read_len(int($meta->{avg_read_length}));
-	$vrlane->qc_status('pending');
-	$vrlane->is_processed(import => 1);
-	$vrlane->update;
-	$vrlane->is_processed(mapped => 1);
-	$vrlane->update;
-	$vrlane->is_processed(qc => 1);
-	$vrlane->update;
-	$vrtrack->transaction_commit();
     }
 }
 
