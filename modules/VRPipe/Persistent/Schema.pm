81473110a2323b3096dc0cc7bb09b6a3235edbf2
Sendu Bala <sb10@sanger.ac.uk>
2012-06-20 11:01:33 +0100

    StepStats->time changed to accept larger numbers - the previous max size was too small for steps that take weeks to run

--- modules/VRPipe/Persistent/Schema.pm
+++ modules/VRPipe/Persistent/Schema.pm
@@ -59,7 +59,7 @@ class VRPipe::Persistent::Schema extends VRPipe::Persistent::SchemaBase {
     use VRPipe::Persistent::SchemaBase;
     use VRPipe::Persistent::ConverterFactory;
 
-    our $VERSION = 18;
+    our $VERSION = 19;
     __PACKAGE__->load_classes({'VRPipe' => [qw/Step Scheduler Job Requirements
                                                DataSource DataElement Pipeline
                                                StepCmdSummary StepMember File
