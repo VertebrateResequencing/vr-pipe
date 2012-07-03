01d113f847e54b356a7b9433ee7f0938f0d68901
Sendu Bala <sb10@sanger.ac.uk>
2012-07-03 12:26:55 +0100

    resolved conflict in merge from develop

--- modules/VRPipe/StepRole.pm
+++ modules/VRPipe/StepRole.pm
@@ -306,14 +306,26 @@ role VRPipe::StepRole {
                     
                     my $wanted_type = $val->type;
                     unless ($wanted_type eq 'any') {
-                        if ($result->s) {
-                            my $type = VRPipe::FileType->create($wanted_type, {file => $result->path});
+                        my $resolved = $result->resolve;
+                        my $has_size = 0;
+                        if ($resolved->s) {
+                            $has_size = 1;
+                            my $type = VRPipe::FileType->create($wanted_type, {file => $resolved->path});
                             unless ($type->check_type) {
-                                push(@skip_reasons, "file ".$result->path." was not the correct type, expected type $wanted_type and got type ".$type->type);
-                                next;
+                                # the type check will fail if the file doesn't
+                                # really exist, so make sure
+                                $resolved->update_stats_from_disc;
+                                if ($resolved->s) {
+                                    push(@skip_reasons, "file ".$result->path." was not the correct type ($wanted_type)");
+                                    next;
+                                }
+                                else {
+                                    $result->update_stats_from_disc;
+                                    $has_size = 0;
+                                }
                             }
                         }
-                        else {
+                        unless ($has_size) {
                             my $db_type = $result->type;
                             if ($db_type && $wanted_type ne $db_type) {
                                 push(@skip_reasons, "file ".$result->path." was not the correct type, expected type $wanted_type and got type $db_type");
@@ -469,7 +481,8 @@ role VRPipe::StepRole {
             my %states_to_restart;
             foreach my $path (@missing) {
                 my $file = VRPipe::File->get(path => $path);
-                next if $file->s; # there's no recourse if the file was actually just missing some metadata, not physically missing
+                my $resolved = $file->resolve;
+                next if $resolved->s; # there's no recourse if the file was actually just missing some metadata, not physically missing
                 my $count = 0;
                 my $state;
                 foreach my $sof (VRPipe::StepOutputFile->search({ file => $file->id })) {
@@ -479,7 +492,7 @@ role VRPipe::StepRole {
                 
                 if ($count == 1) {
                     $with_recourse++;
-                    push(@{$states_to_restart{$state->id}}, $file->path);
+                    push(@{$states_to_restart{$state->id}}, $resolved->path);
                 }
             }
             
