a4d51fa0e9b9d0e2c91b7f7a79a947529d7aea18
Chris joyce <cj5@sanger.ac.uk>
2012-07-17 14:59:36 +0100

    new step bam_metadata_with_sex

--- modules/VRPipe/Pipelines/convex_read_depth_generation.pm
+++ modules/VRPipe/Pipelines/convex_read_depth_generation.pm
@@ -1,30 +1,30 @@
 use VRPipe::Base;
 
-class VRPipe::Pipelines::convex_read_depth_generation with VRPipe::PipelineRole {
+{    #__CLASS VRPipe::Pipelines::convex_read_depth_generation with VRPipe::PipelineRole  #__EXTRA
+    
     method name {
         return 'convex_read_depth_generation';
     }
+    
     method _num_steps {
         return 2;
     }
+    
     method description {
         return 'Run CoNVex pipeline to Generate Read Depth files and add Sample metadata to bam files, for subsequent L2R Generation and CNV Calling pipelines';
     }
+    
     method steps {
         $self->throw("steps cannot be called on this non-persistent object");
     }
     
     method _step_list {
-        return ([ 
-					VRPipe::Step->get(name => 'convex_read_depth'),           #
-					VRPipe::Step->get(name => 'bam_sample_meta'),             #
-				],
-   	            [ 
-					VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 1, to_key => 'bam_files'),
-					VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 2, to_key => 'bam_files'),
-				],
-   	            [ ],
-				);
+        return ([
+             VRPipe::Step->get(name => 'convex_read_depth'), #
+             VRPipe::Step->get(name => 'bam_metadata_with_sex'), #
+            ],
+            [VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 1, to_key => 'bam_files'), VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 2, to_key => 'bam_files'),],
+            [],);
     }
 }
 
---
+++ t/VRPipe/Steps/bam_metadata_with_sex.t
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use File::Copy;
+use Path::Class;
+
+BEGIN {
+    use Test::Most tests => 3;
+    use VRPipeTest (required_env => [qw(VRPIPE_TEST_PIPELINES)],
+                    required_exe => [qw(bamcheck)]);
+    use TestPipelines;
+    
+    use_ok('VRPipe::Steps::bam_metadata');
+}
+
+my ($output_dir, $pipeline, $step) = create_single_step_pipeline('bam_metadata_with_sex', 'bam_files');
+
+my $ds = VRPipe::DataSource->get(type => 'fofn', method => 'all', source => file(qw(t data datasource.bam_fofn))->absolute);
+my $setup = VRPipe::PipelineSetup->get(name        => 'bm_setup',
+                                       datasource  => $ds,
+                                       output_root => $output_dir,
+                                       pipeline    => $pipeline,
+                                       options     => { sample_sex_file => file(qw(t data datasource.sample.sex))->absolute->stringify, });
+ok handle_pipeline(), 'pipeline ran ok';
+
+my $bam = VRPipe::File->get(path => file(qw(t data remapping_bams 8324_8.pe.bam))->absolute);
+my $meta = $bam->metadata;
+is_deeply [$meta->{reads}, $meta->{paired}, $meta->{forward_reads}, $meta->{reads_mapped}, $meta->{sd_insert_size}, $meta->{rmdup_reads}, $meta->{sex}], [500, 1, 250, 233, 66.8, 500, 'M'], 'metadata including sex was correctly applied to a bam file';
+
+finish;
