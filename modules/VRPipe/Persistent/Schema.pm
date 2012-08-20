27d4e80a8d9f76150aa8d3512055bf6ba01cc33d
Sendu Bala <sb10@sanger.ac.uk>
2012-08-20 10:32:33 +0100

    increased the ammount of time we can reserve to over 1000 hrs

--- modules/VRPipe/Persistent/Schema.pm
+++ modules/VRPipe/Persistent/Schema.pm
@@ -60,7 +60,7 @@ class VRPipe::Persistent::Schema extends VRPipe::Persistent::SchemaBase {
     use VRPipe::Persistent::SchemaBase;
     use VRPipe::Persistent::ConverterFactory;
     
-    our $VERSION = 23;
+    our $VERSION = 24;
     __PACKAGE__->load_classes({
             'VRPipe' => [
                 qw/Step Scheduler Job Requirements
