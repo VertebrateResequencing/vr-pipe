bb397fe2266c6b7c9ad6356b3b54923c17ee68dd
Sendu Bala <sb10@sanger.ac.uk>
2012-08-28 13:44:09 +0100

    added copyright info

--- modules/VRPipe/Base/AbstractFactoryMetaClass.pm
+++ modules/VRPipe/Base/AbstractFactoryMetaClass.pm
@@ -1,4 +1,9 @@
 # stolen directly from MooseX::AbstractFactory::Meta::Class
+#
+# This file is copyright (c) 2012 by Mike Whitaker.
+#
+# This is free software; you can redistribute it and/or modify it under the same
+# terms as the Perl 5 programming language system itself.
 
 package VRPipe::Base::AbstractFactoryMetaClass;
 use strict;
