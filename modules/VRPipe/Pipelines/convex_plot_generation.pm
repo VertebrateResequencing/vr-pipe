42ec21eeabd39af4ca03fd07e7536b503de81f78
Chris joyce <cj5@sanger.ac.uk>
2012-10-05 10:02:55 +0100

    convex plot pipeline

---
+++ modules/VRPipe/Pipelines/convex_plot_generation.pm
@@ -0,0 +1,66 @@
+
+=head1 NAME
+
+VRPipe::Pipelines::convex_plot_generation - a pipeline
+
+=head1 DESCRIPTION
+
+This pipeline generates CNV call plots from Convex CNV call text files. The 
+pipeline runs once only for a set of cnv calls; its datasource will probably be
+a vrpipe group_by_metadata set of text files from a convex_cnv_calling pipeline
+or your own fofn of cnv calls.
+
+=head1 AUTHOR
+
+Chris Joyce <cj5@sanger.ac.uk>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2012 Genome Research Limited.
+
+This file is part of VRPipe.
+
+VRPipe is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see L<http://www.gnu.org/licenses/>.
+
+=cut
+
+use VRPipe::Base;
+
+class VRPipe::Pipelines::convex_plot_generation with VRPipe::PipelineRole {
+    method name {
+        return 'convex_plot_generation';
+    }
+    
+    method _num_steps {
+        return 1;
+    }
+    
+    method description {
+        return 'Run CoNVex pipeline to Generate CNV call plots from set of Convex CNV calls';
+    }
+    
+    method steps {
+        $self->throw("steps cannot be called on this non-persistent object");
+    }
+    
+    method _step_list {
+        return ([
+                VRPipe::Step->get(name => 'convex_plots'), #
+            ],
+            [VRPipe::StepAdaptorDefiner->new(from_step => 0, to_step => 1, to_key => 'cnv_files'),],
+            [],
+        );
+    }
+}
+
+1;
---
+++ t/VRPipe/Pipelines/run_convex_plots.t
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Path::Class;
+
+BEGIN {
+    use Test::Most tests => 3;
+    use VRPipeTest (
+        required_env => [qw(VRPIPE_TEST_PIPELINES CONVEX_R_LIB)],
+        max_retries  => 1,
+        required_exe => [qw(R)]
+    );
+    use TestPipelines;
+}
+
+my $output_dir = get_output_dir('convex_plot_generation_pipeline');
+
+ok my $pipeline1 = VRPipe::Pipeline->create(name => 'convex_plot_generation'), 'able to create the convex_plot_generation pipeline';
+
+my @s_names;
+foreach my $stepmember ($pipeline1->steps) {
+    push(@s_names, $stepmember->step->name);
+}
+
+my @expected_step_names = qw(convex_plots);
+is_deeply \@s_names, \@expected_step_names, 'the rd pipeline has the correct steps';
+
+my $convex_r_libs = $ENV{CONVEX_R_LIB};
+
+my $fofn = file(qw(t data cnv cnv.calls.fofn));
+
+my $pipelinesetup1 = VRPipe::PipelineSetup->create(
+    name        => 'convex_plot_generation_pipeline',
+    datasource  => VRPipe::DataSource->create(type => 'fofn', method => 'group_all', source => $fofn),
+    output_root => $output_dir,
+    pipeline    => $pipeline1,
+    options     => {
+        cleanup => 0,
+        r_libs => "$convex_r_libs",
+    }
+);
+
+my (@output_files, @final_files);
+my @output_subdirs = output_subdirs(1);
+push (@output_files, file(@output_subdirs, '1_convex_plots','CnvFofn.txt'));
+push (@output_files, file(@output_subdirs, '1_convex_plots','CNVstats_CallsperSample.png'));
+push (@output_files, file(@output_subdirs, '1_convex_plots','CNVstats_DelDupRatio.png'));
+
+ok handle_pipeline(@output_files, @final_files), 'rd pipeline ran and created all expected output files';
+
+done_testing;
