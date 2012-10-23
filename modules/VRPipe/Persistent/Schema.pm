225abea7f9f708b92cada47fa97a08ed76358479
Sendu Bala <sb10@sanger.ac.uk>
2012-10-23 13:35:48 +0100

    bump schema version now that we have new tables, amongst other changes

--- modules/VRPipe/Persistent/Schema.pm
+++ modules/VRPipe/Persistent/Schema.pm
@@ -60,7 +60,7 @@ class VRPipe::Persistent::Schema extends VRPipe::Persistent::SchemaBase {
     use VRPipe::Persistent::SchemaBase;
     use VRPipe::Persistent::ConverterFactory;
     
-    our $VERSION = 25;
+    our $VERSION = 26;
     __PACKAGE__->load_classes({
             'VRPipe' => [
                 qw/Step Scheduler Job Requirements
