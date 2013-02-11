65f5b4c4bd8cc150fd37db050d15afa434f5cee5
Sendu Bala <sb10@sanger.ac.uk>
2013-02-11 13:33:46 +0000

    adding initial not-yet-implemented outline of ec2 scheduler module

---
+++ modules/VRPipe/Schedulers/ec2.pm
@@ -0,0 +1,175 @@
+
+=head1 NAME
+
+VRPipe::Schedulers::lsf - interface to LSF
+
+=head1 SYNOPSIS
+
+*** more documentation to come
+
+=head1 DESCRIPTION
+
+This class provides L<Amazon
+EC2|http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Welcome.html>-specific
+command lines for use by L<VRPipe::Scheduler>.
+
+For this to work you must have exactly one AMI with the name 'vrpipe-install',
+and it must be private to you. You can create such an AMI by starting with our
+public AMI and customising for your needs. The AMI must boot up to an
+environment with a working VRPipe installation and all the software you need to
+run. The VRPipe installation must be configured to use ec2 as the job
+scheduler, and you must have provided the access and secret keys (which can be
+found at https://portal.aws.amazon.com/gp/aws/securityCredentials?).
+
+You must also have a security group called 'vrpipe-security' which at a minimum
+must allow ssh between ec2 instances.
+
+=head1 AUTHOR
+
+Sendu Bala <sb10@sanger.ac.uk>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2013 Genome Research Limited.
+
+This file is part of VRPipe.
+
+VRPipe is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see L<http://www.gnu.org/licenses/>.
+
+=cut
+
+use VRPipe::Base;
+
+class VRPipe::Schedulers::ec2 with VRPipe::SchedulerMethodsRole {
+    use VM::EC2;
+    
+    our %queues;
+    our ($ami, $access_key, $secret_key, $region);
+    
+    method start_command {
+        return 'sleep 1'; #*** not really applicable
+    }
+    
+    method stop_command {
+        return 'sleep 1'; #*** not really applicable
+    }
+    
+    method submit_command {
+        return 'perl -MVRPipe::Scheduler::ec2 -e "VRPipe::Scheduler::ec2->submit(@ARGV)"'; # we call a method in this module to submit
+    }
+    
+    method submit_args (VRPipe::Requirements :$requirements!, Str|File :$stdo_file!, Str|File :$stde_file!, Str :$cmd!, VRPipe::PersistentArray :$array?) {
+        # access the requirments object and build up the string based on memory,
+        # time, cpu etc.
+        my $queue = $self->determine_queue($requirements);
+        # *** ...
+        my $megabytes          = $requirements->memory;
+        my $m                  = $megabytes * 1000;
+        my $requirments_string = "-q $queue -M$m -R 'select[mem>$megabytes] rusage[mem=$megabytes]'";
+        my $cpus               = $requirements->cpus;
+        if ($cpus > 1) {
+            $requirments_string .= " -n$cpus -R 'span[hosts=1]'";
+        }
+        
+        return qq[$requirments_string '$cmd'];
+    }
+    
+    method determine_queue (VRPipe::Requirements $requirements) {
+        # based on the requirements we want to select an appropriate ec2
+        # instance type to run the job on
+        
+        #my $megabytes = $requirements->memory;
+        #my $chosen_queue;
+        #*** not yet implemented
+        #return $chosen_queue;
+        
+        #*** for now we just hard-code m1.medium, which is 1 core, 3.7GB
+        
+        return 'm1.medium';
+    }
+    
+    method queue_time (VRPipe::Requirements $requirements) {
+        # we can run for an unlimited time on all instance types
+        return 31536000;
+    }
+    
+    method switch_queue (PositiveInt $sid, Str $new_queue) {
+        # we don't support queue switching
+        $self->throw("Queue Switching is not supported (and should not be necessary) for the ec2 scheduler");
+    }
+    
+    method get_1based_index (Maybe[PositiveInt] $index?) {
+        # we don't have any concept of a job 'array', so don't deal with indexes
+        return 1;
+    }
+    
+    method get_sid (Str $cmd) {
+        my $ec2;
+        unless ($ami) {
+            # get user's private vrpipe-install AMI if we don't already know it
+            $ec2 = VM::EC2->new(-access_key => $access_key, -secret_key => $secret_key, -region => $region);
+            my @amis = $ec2->describe_images(-owner => "self", -filter => { name => "vrpipe-install" }) or $self->throw("desribe_images call failed: " . $ec2->error_str);
+            unless (@amis == 1) {
+                $self->throw("There was not exactly 1 vrpipe-install AMI owned by you");
+            }
+            $ami = $amis[0]->imageId;
+        }
+        
+        #*** not yet implemented
+        
+        #my $output = `$cmd`;
+        #my ($sid) = $output =~ /Job \<(\d+)\> is submitted/;
+        
+        if ($sid) {
+            return $sid;
+        }
+        else {
+            $self->throw("Failed to submit to scheduler");
+        }
+    }
+    
+    method kill_sid (PositiveInt $sid, Int $aid, PositiveInt $secs = 30) {
+        #*** not yet implemented
+        
+        my $t = time();
+        while (1) {
+            last if time() - $t > $secs;
+            
+            #*** fork and kill child if over time limit?
+            my $status = $self->sid_status($sid, $aid);
+            last if ($status eq 'UNKNOWN' || $status eq 'DONE' || $status eq 'EXIT');
+            
+            #system("bkill $id");
+            
+            sleep(1);
+        }
+        return 1;
+    }
+    
+    method all_status {
+        my %status = ();
+        
+        #*** not yet implemented
+        # $status{$sid} = 'RUN';
+        
+        return %status;
+    }
+    
+    method sid_status (PositiveInt $sid, Int $aid) {
+        my $status;
+        #*** not yet implemented
+        return $status || 'UNKNOWN'; # *** needs to return a word in a defined vocabulary suitable for all schedulers
+    }
+}
+
+1;
