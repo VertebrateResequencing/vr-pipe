710dc9d73ef995aeff4c425eb030ea73f25b9f6b
Sendu Bala <sb10@sanger.ac.uk>
2013-02-25 10:45:06 +0000

    added log_event() calls; tried to fill in edge case where a claim_and_run is supposdily successful, yet claim did not get set in db and then the job dies

--- modules/VRPipe/Submission.pm
+++ modules/VRPipe/Submission.pm
@@ -145,6 +145,7 @@ class VRPipe::Submission extends VRPipe::Persistent {
                         # claim: clean this up now
                         $sub_to_claim->_claim(0);
                         $sub_to_claim->update;
+                        $self->stepstate->pipelinesetup->log_event("claim_and_run() found that the Submission was claimed yet the Job was dead and had a heartbeat: the Job must have died so the claim was unset and will reset the Job", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $job->id);
                         $sub_to_claim->_reset_job;
                     }
                     else {
@@ -165,13 +166,21 @@ class VRPipe::Submission extends VRPipe::Persistent {
                                 $started_running = 1;
                                 last;
                             }
+                            
+                            #*** there's also some kind of issue that we can
+                            # have no heartbeat yet ran the Job successfully
+                            # already, and we might also have set Submission to
+                            # done... not sure what to do about failures to
+                            # update database as expected...
                         }
                         
                         unless ($started_running) {
                             $sub_to_claim->_claim(0);
                             $sub_to_claim->update;
+                            $self->stepstate->pipelinesetup->log_event("claim_and_run() found that the Submission was claimed yet the Job was dead and had no heartbeat; after waiting 60s the Job still hasn't started so the claim was unset", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $job->id);
                             
                             unless (defined $job->exit_code && $job->end_time) {
+                                $self->stepstate->pipelinesetup->log_event("claim_and_run() found that the Submission was claimed yet the Job was dead and had no heartbeat; after waiting 60s the Job had no end_time or exit_code so the Job will be reset", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $job->id);
                                 $sub_to_claim->_reset_job;
                             }
                         }
@@ -183,9 +192,24 @@ class VRPipe::Submission extends VRPipe::Persistent {
                 $claimed_by_us = 0;
             }
             else {
-                $sub_to_claim->_claim(1);
-                $sub_to_claim->update;
-                $claimed_by_us = 1;
+                # ok, looks like we're good to get the claim, but there's an
+                # edge case where another process started running the job yet
+                # database weirdness prevented the claim from being set to 1,
+                # and then the process died before job could be updated; check
+                # that the job isn't dead
+                if ($job->alive(no_suicide => 1)) {
+                    $sub_to_claim->_claim(1);
+                    $sub_to_claim->update;
+                }
+                elsif ($job->start_time) {
+                    $self->stepstate->pipelinesetup->log_event("claim_and_run() found that the Submission was unclaimed yet the Job is dead having started: will reset the Job", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $job->id);
+                    $sub_to_claim->_reset_job;
+                }
+                else {
+                    $sub_to_claim->_claim(1);
+                    $sub_to_claim->update;
+                    $claimed_by_us = 1;
+                }
             }
         };
         $self->do_transaction($transaction, "Failed when trying to claim submission");
@@ -199,16 +223,19 @@ class VRPipe::Submission extends VRPipe::Persistent {
         my $transaction = sub {
             if ($self->_get_claim) {
                 if ($self->failed) {
+                    $self->stepstate->pipelinesetup->log_event("claim_and_run() failed to get the claim, and since the Submission is failed will retry()", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
                     $self->retry;
                     $run_ok = 0;
                 }
                 else {
                     # start running the associated job
+                    $self->stepstate->pipelinesetup->log_event("claim_and_run() got the claim so will call Job->run()", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
                     $run_ok = $self->job->run(submission => $self, $allowed_time ? (allowed_time => $allowed_time) : ());
                 }
                 
                 unless ($run_ok && $self->job->start_time) {
                     $self->release;
+                    $self->stepstate->pipelinesetup->log_event("claim_and_run() got the claim but Job->run did not result in the Job starting, so the claim was released", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
                 }
             }
         };
@@ -240,15 +267,16 @@ class VRPipe::Submission extends VRPipe::Persistent {
         if ($self->job->ok) {
             $self->_done(1);
             $self->_failed(0);
+            $self->stepstate->pipelinesetup->log_event("update_status() call found that the Job was ok, so done set to 1", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
         }
         else {
             $self->_done(0);
             $self->_failed(1);
+            $self->stepstate->pipelinesetup->log_event("update_status() call found that the Job was not ok, so failed set to 1", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
         }
+        $self->update;
         
         $self->archive_output;
-        
-        $self->update;
     }
     
     method archive_output {
@@ -266,7 +294,8 @@ class VRPipe::Submission extends VRPipe::Persistent {
                 my $path = file($std_dir, 'job_stderr');
                 my $vrfile = VRPipe::File->create(path => $path, type => 'cat');
                 my $vrfile_id = $vrfile ? $vrfile->id : 'no_id';
-                $self->throw("toa $count had dest $dest compared to source " . $source->path . "; should have been $path for vrfile $vrfile_id");
+                $self->warn("toa $count had dest $dest compared to source " . $source->path . "; should have been $path for vrfile $vrfile_id");
+                return;
             }
             $self->concatenate(
                 $source, $dest,
@@ -316,6 +345,8 @@ class VRPipe::Submission extends VRPipe::Persistent {
         }
         
         $extra = ceil($extra / 100) * 100;
+        
+        $self->stepstate->pipelinesetup->log_event("extra_memory() call will change our Requirements by adding ${extra}MB", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
         $self->_add_extra('memory', $extra);
     }
     
@@ -324,6 +355,7 @@ class VRPipe::Submission extends VRPipe::Persistent {
     }
     
     method extra_time (Int $extra = 3600) {
+        $self->stepstate->pipelinesetup->log_event("extra_time() call will change our Requirements by adding ${extra}secs", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
         $self->_add_extra('time', $extra);
     }
     
@@ -412,6 +444,8 @@ class VRPipe::Submission extends VRPipe::Persistent {
     method retry {
         return unless ($self->done || $self->failed);
         
+        $self->stepstate->pipelinesetup->log_event("Submission->retry() call will reset our Job and the Submission", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
+        
         # reset the job
         $self->_reset_job;
         
@@ -438,6 +472,7 @@ class VRPipe::Submission extends VRPipe::Persistent {
     method _reset_job {
         my $job = $self->job;
         if ($job->start_time && !$job->end_time) {
+            $self->stepstate->pipelinesetup->log_event("Submission->_reset_job() call will kill the currently running Job", dataelement => $self->stepstate->dataelement->id, stepstate => $self->stepstate->id, submission => $self->id, job => $self->job->id);
             $job->kill_job($self);
         }
         
