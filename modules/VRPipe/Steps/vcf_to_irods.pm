0833f707b94512cc98ecb29ea959fbba51632fd3
Sendu Bala <sb10@sanger.ac.uk>
2013-04-12 09:49:34 +0100

    disconnect before running cmds

--- modules/VRPipe/Steps/vcf_to_irods.pm
+++ modules/VRPipe/Steps/vcf_to_irods.pm
@@ -5,8 +5,9 @@ VRPipe::Steps::vcf_to_irods - a step
 
 =head1 DESCRIPTION
 
-Adds vcf files and associated metadata to irods. Requires that the vcfs have sample(s) and md5 checksum vrpipe metadata.
-The vcfs are added under an irods root collection using a directory structure constructed from the md5 checksum.
+Adds vcf files and associated metadata to irods. Requires that the vcfs have
+sample(s) and md5 checksum vrpipe metadata. The vcfs are added under an irods
+root collection using a directory structure constructed from the md5 checksum.
 Also optionally adds the vcf index to irods.
 
 =head1 AUTHOR
@@ -36,17 +37,16 @@ this program. If not, see L<http://www.gnu.org/licenses/>.
 use VRPipe::Base;
 
 class VRPipe::Steps::vcf_to_irods with VRPipe::StepRole {
-    
     method options_definition {
         return {
-            'irods_root' => VRPipe::StepOption->create( description  => 'irods root path to which to add objects'),
-            'has_index' => VRPipe::StepOption->create( description  => 'boolean, indicates vcf has an index which needs also adding to irods', optional => 1, default_value => 1),
-            'study' => VRPipe::StepOption->create( description  => 'study to which vcf belongs, to be added as irods metadata, if not found in vcf metadata', optional => 1),
-            'release' => VRPipe::StepOption->create( description  => 'release to which vcf belongs, to be added irods as metadata, if not found in vcf metadata', optional => 1),
-            'update' => VRPipe::StepOption->create( description  => 'boolean, indicates program will not fail if vcf is already in irods, will overwrite and add any additional metadata ', optional => 1, default_value => 1),
+            'irods_root' => VRPipe::StepOption->create(description => 'irods root path to which to add objects'),
+            'has_index'  => VRPipe::StepOption->create(description => 'boolean, indicates vcf has an index which needs also adding to irods', optional => 1, default_value => 1),
+            'study'   => VRPipe::StepOption->create(description => 'study to which vcf belongs, to be added as irods metadata, if not found in vcf metadata',                              optional => 1),
+            'release' => VRPipe::StepOption->create(description => 'release to which vcf belongs, to be added irods as metadata, if not found in vcf metadata',                            optional => 1),
+            'update'  => VRPipe::StepOption->create(description => 'boolean, indicates program will not fail if vcf is already in irods, will overwrite and add any additional metadata ', optional => 1, default_value => 1),
         };
     }
-
+    
     method inputs_definition {
         return {
             vcf_files => VRPipe::StepIODefinition->create(
@@ -61,21 +61,20 @@ class VRPipe::Steps::vcf_to_irods with VRPipe::StepRole {
         return sub {
             my $self = shift;
             
-            my $options = $self->options;
+            my $options    = $self->options;
             my $irods_root = $options->{irods_root};
             $irods_root =~ s/\/$//;
             my $has_index = $options->{has_index};
-            my $study = $options->{study};
-            my $release = $options->{release};
-            my $update = $options->{update};
+            my $study     = $options->{study};
+            my $release   = $options->{release};
+            my $update    = $options->{update};
             
             my $req = $self->new_requirements(memory => 100, time => 1);
-
+            
             foreach my $vcf_file (@{ $self->inputs->{vcf_files} }) {
-
                 my $input_path = $vcf_file->path;
-                my $basename = $vcf_file->basename;
-
+                my $basename   = $vcf_file->basename;
+                
                 my $cmd = "use VRPipe::Steps::vcf_to_irods; VRPipe::Steps::vcf_to_irods->add_vcf_to_irods('$input_path', '$irods_root', '$has_index','$study', '$release', '$update');";
                 $self->dispatch_vrpipecode($cmd, $req);
             }
@@ -83,7 +82,7 @@ class VRPipe::Steps::vcf_to_irods with VRPipe::StepRole {
     }
     
     method outputs_definition {
-        return { };
+        return {};
     }
     
     method post_process_sub {
@@ -95,77 +94,78 @@ class VRPipe::Steps::vcf_to_irods with VRPipe::StepRole {
     }
     
     method add_vcf_to_irods (ClassName|Object $self: Str|File $input_vcf_path, Str $irods_root, Bool $has_index , Str $study, Str $release, Bool $update) {
-
         my $vcf_file = VRPipe::File->get(path => $input_vcf_path);
-
+        
         # Add collection using directory structure constructed from md5 checksum, eg
         # eg 198f9d136fcd9b7f83b3bf32dc25f181: /uk10k/uk10k/STUDY/1/9/8/f/d136fcd9b7f83b3bf32dc25f181
-
+        
         my $vcf_meta = $vcf_file->metadata;
         $study = $vcf_meta->{study} if $vcf_meta->{study};
         $self->throw("Must provde a study either in metadata or as param") unless $study;
-
+        
         my $md5 = $vcf_meta->{md5};
         $self->throw("Could not get md5 checksum metadata for $input_vcf_path") unless $md5;
-
-        my ($md51,$md52,$md53,$md54) = split(//,$md5);
-        my $md55=substr($md5,4);
-        my $idir="$irods_root/$study/$md51/$md52/$md53/$md54/$md55";
-
+        
+        my ($md51, $md52, $md53, $md54) = split(//, $md5);
+        my $md55 = substr($md5, 4);
+        my $idir = "$irods_root/$study/$md51/$md52/$md53/$md54/$md55";
+        
         #add collection and object
+        $vcf_file->disconnect;
         system("imkdir", "-p", "$idir") && $self->throw("failed to imkdir $idir");
-
+        
         my $iput_cmd = "iput";
         $iput_cmd .= " -f" if $update;
-
+        
         system("$iput_cmd $input_vcf_path $idir") && $self->throw("failed to $iput_cmd $input_vcf_path $idir");
         if ($has_index) {
             system("$iput_cmd $input_vcf_path.tbi $idir") && $self->throw("failed to $iput_cmd $input_vcf_path.tbi $idir");
         }
-
+        
         my $basename = $vcf_file->basename;
+        $vcf_file->disconnect;
         system("ils", "-r", "$idir/$basename") && $self->throw("failed to ils $idir/$basename");
-
+        
         # Add metadata
         my %meta;
-
+        
         my @cmd = ("imeta", "add", "-d", "$idir/$basename", "type", "vcf");
         if (system(@cmd)) {
             $self->throw("failed to run '@cmd'") unless $update;
         }
-        $meta{type}{vcf}=0;
-
+        $meta{type}{vcf} = 0;
+        
         @cmd = ("imeta", "add", "-d", "$idir/$basename", "md5", "$md5");
         if (system(@cmd)) {
             $self->throw("failed to run '@cmd'") unless $update;
         }
-        $meta{md5}{$md5}=0;
-
+        $meta{md5}{$md5} = 0;
+        
         # sample names are catanated into a string and delimited by '#'
-        my @samples = split(/#/,$vcf_meta->{samples});
-        foreach my $sample(@samples) {
+        my @samples = split(/#/, $vcf_meta->{samples});
+        foreach my $sample (@samples) {
             @cmd = ("imeta", "add", "-d", "$idir/$basename", "sample", "$sample");
             if (system(@cmd)) {
                 $self->throw("failed to run '@cmd'") unless $update;
             }
-            $meta{sample}{$sample}=0;
+            $meta{sample}{$sample} = 0;
         }
-
+        
         @cmd = ("imeta", "add", "-d", "$idir/$basename", "study", "$study");
         if (system(@cmd)) {
             $self->throw("failed to run '@cmd'") unless $update;
         }
-        $meta{study}{$study}=0;
-
+        $meta{study}{$study} = 0;
+        
         $release = $vcf_meta->{release} if $vcf_meta->{release};
         if ($release) {
             @cmd = ("imeta", "add", "-d", "$idir/$basename", "release", "$release");
             if (system(@cmd)) {
                 $self->throw("failed to run '@cmd'") unless $update;
             }
-            $meta{release}{$release}=0;
+            $meta{release}{$release} = 0;
         }
-
+        
         # metadata check
         my $pipe = "imeta ls -d $idir/$basename |";
         my $fh;
@@ -176,19 +176,19 @@ class VRPipe::Steps::vcf_to_irods with VRPipe::StepRole {
             $meta{$meta_key}{$1}++ if /value: (\S*)$/;
         }
         close($fh);
-
+        
         foreach my $k1 (keys %meta) {
-            foreach my $k2 ( keys %{$meta{$k1}} ) {
+            foreach my $k2 (keys %{ $meta{$k1} }) {
                 $self->throw("Could not get $k1 $k2 metadata for $idir/$basename") unless $meta{$k1}{$k2} > 0;
             }
         }
         return 1;
     }
-
+    
     method max_simultaneous {
         return 50;
     }
-    
+
 }
 
 1;
