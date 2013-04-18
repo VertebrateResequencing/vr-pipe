7144998c7e259e5a394b4d6b9cb51f0d453a43bb
Sendu Bala <sb10@sanger.ac.uk>
2013-04-18 13:08:10 +0100

    we now require Proc::Daemon

--- build_lib/VRPipeBuild.pm
+++ build_lib/VRPipeBuild.pm
@@ -105,6 +105,7 @@ sub required_modules {
         'Path::Class'                    => 0,
         'Perl6::Form'                    => 0,
         'POSIX'                          => 0,
+        'Proc::Daemon'                   => 0,
         'Proc::Killfam'                  => 0,
         'Redis'                          => 1.9,
         'Storable'                       => 0,
