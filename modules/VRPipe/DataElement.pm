200243f745848a06ba7aed4591b434792495e67d
Sendu Bala <sb10@sanger.ac.uk>
2013-05-11 19:59:34 +0100

    DataElement result column removed, replaced with filelist and keyvallist columns, with new metadata() method and result() method for backwards compatability

--- modules/VRPipe/DataElement.pm
+++ modules/VRPipe/DataElement.pm
@@ -50,11 +50,20 @@ class VRPipe::DataElement extends VRPipe::Persistent {
         belongs_to => 'VRPipe::DataSource'
     );
     
-    has 'result' => (
-        is     => 'rw',
-        isa    => 'HashRef',
-        traits => ['VRPipe::Persistent::Attributes'],
-        is_key => 1
+    has 'filelist' => (
+        is         => 'rw',
+        isa        => Persistent,
+        traits     => ['VRPipe::Persistent::Attributes'],
+        is_key     => 1,
+        belongs_to => 'VRPipe::FileList'
+    );
+    
+    has 'keyvallist' => (
+        is         => 'rw',
+        isa        => Persistent,
+        traits     => ['VRPipe::Persistent::Attributes'],
+        is_key     => 1,
+        belongs_to => 'VRPipe::KeyValList'
     );
     
     has 'withdrawn' => (
@@ -71,16 +80,30 @@ class VRPipe::DataElement extends VRPipe::Persistent {
     }
     
     method files {
-        my $paid = $self->result->{paths} || return;
-        if (ref($paid)) {
-            # we used to store an array ref of file path strings in the db
-            return [map { VRPipe::File->get(path => $_) } @$paid];
-        }
-        return [VRPipe::PersistentArray->get(id => $paid)->member_instances];
+        my $filelist = $self->filelist;
+        return [$filelist->files];
     }
     
     method paths {
         return map { $_->path->stringify } @{ $self->files || return };
+    }
+    
+    method metadata {
+        my $keyvallist = $self->keyvallist;
+        return $keyvallist->as_hashref;
+    }
+    
+    method result {
+        my $result = {};
+        my @paths  = $self->paths;
+        if (@paths) {
+            $result->{paths} = \@paths;
+        }
+        my $meta = $self->metadata;
+        while (my ($key, $val) = each %{$meta}) {
+            $result->{$key} = $val;
+        }
+        return $result;
     }
     
     method start_from_scratch (VRPipe::PipelineSetup $setup, ArrayRef[PositiveInt] $step_numbers?) {
@@ -89,40 +112,49 @@ class VRPipe::DataElement extends VRPipe::Persistent {
     
     # most datasources have {result}->{paths} which contains absolute file path
     # strings. We convert these to VRPipe::File objects, and then to a
-    # PersistentArray and store just the id. This saves db space and ensures we
-    # can match the result regardless of order of paths, reducing spurious
+    # FileList and store just the id. This saves db space and ensures we
+    # can match the result regardless of order of paths, preventing spurious
     # DataElement withdrawal and recreation
     around bulk_create_or_update (ClassName|Object $self: @args) {
         foreach my $args (@args) {
-            $self->_deflate_paths($args->{result});
+            $self->_convert_result($args);
         }
         
         return $self->$orig(@args);
     }
     
     around _get (ClassName|Object $self: Bool $create, %args) {
-        if (defined $args{result}) {
-            $self->_deflate_paths($args{result});
-        }
-        
+        $self->_convert_result(\%args);
         return $self->$orig($create, %args);
     }
     
     around search_rs (ClassName|Object $self: HashRef $search_args, Maybe[HashRef] $search_attributes?) {
-        if (defined $search_args->{result}) {
-            $self->_deflate_paths($search_args->{result});
-        }
+        $self->_convert_result($search_args);
         
         my @args = ($search_args);
         push(@args, $search_attributes) if $search_attributes;
         return $self->$orig(@args);
     }
     
-    sub _deflate_paths {
-        my ($self, $result) = @_;
-        if (defined $result->{paths} && ref($result->{paths})) {
-            $result->{paths} = VRPipe::PersistentArray->get(members => [map { VRPipe::File->get(path => $_) } @{ $result->{paths} }], any_order => 1)->id;
+    sub _convert_result {
+        my ($self, $args) = @_;
+        return unless exists $args->{result};
+        my $result = delete $args->{result};
+        
+        # convert paths in the result to a filelist
+        my $paths = $result->{paths};
+        if (defined $paths && ref($paths)) {
+            $args->{filelist} = VRPipe::FileList->get(files => [map { VRPipe::File->get(path => $_) } @{$paths}])->id;
         }
+        $args->{filelist} ||= VRPipe::FileList->get(files => [])->id;
+        
+        # convert anything remaining in the result to a keyvals
+        my $hash = {};
+        while (my ($key, $val) = each %{$result}) {
+            next if $key eq 'paths';
+            $hash->{$key} = $val;
+        }
+        $args->{keyvallist} = VRPipe::KeyValList->get(hash => $hash)->id;
     }
 }
 
