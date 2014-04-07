dc9efb487c80b4429257404afd21d671ed22e8e2
Sendu Bala <sb10@sanger.ac.uk>
2014-04-07 17:36:43 +0100

    removed defunct fcr_to_vcf() method; vcf_sample_from_metadata option now supports multi keys and defaults to irods keys; new bcftools_exe option; now records that we output tbi_files

--- modules/VRPipe/Steps/genome_studio_fcr_to_vcf.pm
+++ modules/VRPipe/Steps/genome_studio_fcr_to_vcf.pm
@@ -43,14 +43,19 @@ class VRPipe::Steps::genome_studio_fcr_to_vcf with VRPipe::StepRole {
     method options_definition {
         return {
             vcf_sample_from_metadata => VRPipe::StepOption->create(
-                description   => 'if the id in the second column of the fcr matches metadata with key x, but you want the sample id in the VCF to have metadata from key y, provide x:y',
+                description   => 'if the id in the second column of the fcr matches metadata with key x, but you want the sample id in the VCF to have metadata from key y, provide x:y; separate multiple y keys with + symbols - values will be joined with underscores',
                 optional      => 1,
-                default_value => 'library:sample'
+                default_value => 'infinium_sample:public_name+sample'
             ),
             fcr_to_vcf_exe => VRPipe::StepOption->create(
                 description   => 'path to the fcr-to-vcf executable',
                 optional      => 1,
                 default_value => 'fcr-to-vcf'
+            ),
+            bcftools_exe => VRPipe::StepOption->create(
+                description   => 'path to the bcftools executable (used by fcr-to-vcf)',
+                optional      => 1,
+                default_value => 'bcftools'
             )
         };
     }
@@ -77,6 +82,11 @@ class VRPipe::Steps::genome_studio_fcr_to_vcf with VRPipe::StepRole {
                 description => 'VCF file',
                 max_files   => -1,
                 metadata    => { sample => 'sample name for cell line' }
+            ),
+            tbi_files => VRPipe::StepIODefinition->create(
+                type        => 'bin',
+                description => 'tbi file',
+                max_files   => -1
             )
         };
     }
@@ -86,12 +96,17 @@ class VRPipe::Steps::genome_studio_fcr_to_vcf with VRPipe::StepRole {
             my $self       = shift;
             my $options    = $self->options;
             my $fcr_to_vcf = $options->{fcr_to_vcf_exe};
+            my $bcftools   = $options->{bcftools_exe};
             my $sfm        = $options->{vcf_sample_from_metadata};
-            my ($src_key, $dst_key);
+            my ($src_key, @dst_keys);
             my $summary_s = '';
             if ($sfm) {
-                ($src_key, $dst_key) = split(':', $sfm);
+                ($src_key, my $dst_key) = split(':', $sfm);
+                @dst_keys = split(/\+/, $dst_key);
                 $summary_s = " -s $sfm";
+            }
+            else {
+                $summary_s = " -m";
             }
             
             my ($map_file) = @{ $self->inputs->{map_file} };
@@ -102,26 +117,34 @@ class VRPipe::Steps::genome_studio_fcr_to_vcf with VRPipe::StepRole {
                 VRPipe::StepCmdSummary->create(
                     exe     => 'fcr-to-vcf',
                     version => 0,
-                    summary => "cat \$fcr_file | $fcr_to_vcf -a \$map_file$summary_s -o \$outdir"
+                    summary => "cat \$fcr_file | $fcr_to_vcf -b $bcftools -a \$map_file$summary_s -o \$outdir"
                 )
             );
             
             foreach my $fcr_file (@{ $self->inputs->{fcr_files} }) {
                 my $meta = $fcr_file->metadata;
                 
-                my $outdir = 'output';
-                my $s      = '';
-                if ($src_key && $dst_key && defined $meta->{$src_key} && defined $meta->{$dst_key}) {
-                    $s      = " -s $meta->{$src_key}:$meta->{$dst_key}";
-                    $outdir = $meta->{$dst_key};
+                my $outdir = $meta->{sample};
+                my $basename;
+                my $s = '';
+                if ($src_key && @dst_keys && defined $meta->{$src_key} && defined $meta->{ $dst_keys[0] }) {
+                    my $dst_vals = join('_', map { $meta->{$_} || 'undef' } @dst_keys);
+                    $s        = " -s $meta->{$src_key}:$dst_vals";
+                    $outdir   = $dst_vals;
+                    $basename = file($outdir, "$outdir.vcf.gz")->stringify;
+                }
+                else {
+                    $s        = ' -m';
+                    $basename = "$outdir.vcf.gz";
                 }
                 
                 #$self->output_file(basename => $basename, type => 'vcf', temporary => 1);
                 
-                my $vcf_file_path = $self->output_file(output_key => 'vcf_files', basename => file($outdir, "$outdir.vcf.gz"), type => 'vcf', metadata => $meta)->path;
+                my $vcf_file_path = $self->output_file(output_key => 'vcf_files', basename => $basename, type => 'vcf', metadata => $meta)->path;
+                $self->output_file(output_key => 'tbi_files', basename => $basename . '.tbi', type => 'tbi');
                 my $fcr_file_path = $fcr_file->path;
                 
-                $self->dispatch(["cat $fcr_file_path | $fcr_to_vcf -a $map_file$s -o $outdir", $req]);
+                $self->dispatch(["cat $fcr_file_path | $fcr_to_vcf -b $bcftools -a $map_file$s -o $outdir", $req]);
             }
         };
     }
@@ -136,123 +159,6 @@ class VRPipe::Steps::genome_studio_fcr_to_vcf with VRPipe::StepRole {
     
     method description {
         return "Convert single-sample genotype data text files (FCR files) to sorted compressed VCF suitable for calling with.";
-    }
-    
-    method fcr_to_vcf (ClassName|Object $self: Str|File :$fcr, Str|File :$vcf, Str|File :$snp_manifest, Str :$vcf_sort, Str :$bgzip) {
-        open(my $mfh, '<', $snp_manifest) || die "Could not read from $snp_manifest\n";
-        my $fcr_file = VRPipe::File->get(path => $fcr);
-        my $vcf_file = VRPipe::File->get(path => $vcf);
-        my $vcf_file_unsorted = $vcf;
-        $vcf_file_unsorted =~ s/\.vcf.gz$/.unsorted.vcf/;
-        $vcf_file_unsorted = VRPipe::File->get(path => $vcf_file_unsorted);
-        my $ofh = $vcf_file_unsorted->openw;
-        
-        # get date and sample name for VCF header; the fcr file may have the
-        # library name instead of sample name, so get that as well
-        my $dt      = DateTime->now;
-        my $date    = $dt->ymd('');
-        my $sample  = $fcr_file->meta_value('sample');
-        my $library = $fcr_file->meta_value('library');
-        $vcf_file->disconnect;
-        
-        # print VCF header
-        print $ofh "##fileformat=VCFv4.0\n";
-        print $ofh "##fileDate=$date\n";
-        print $ofh "##source=$fcr HipSci genotyping file\n";
-        print $ofh "##INFO=<ID=NS,Number=1,Type=Integer,Description=\"Number of Samples With Data\">\n";
-        print $ofh "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n";
-        print $ofh "##FORMAT=<ID=GC,Number=1,Type=Float,Description=\"GenCall score\">\n";
-        print $ofh "##FORMAT=<ID=IA,Number=1,Type=Float,Description=\"Intensity of the A Allele\">\n";
-        print $ofh "##FORMAT=<ID=IB,Number=1,Type=Float,Description=\"Intensity of the B Allele\">\n";
-        print $ofh "##FORMAT=<ID=BAF,Number=1,Type=Float,Description=\"B Allele Frequency\">\n";
-        print $ofh "##FORMAT=<ID=LRR,Number=1,Type=Float,Description=\"Log R Ratio\">\n";
-        print $ofh "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t$sample\n";
-        
-        # read the snp manifest data into a hash
-        my %manifest;
-        while (<$mfh>) {
-            chomp;
-            my @vals = split(/\t/);
-            next if $vals[5] eq 'UNKNOWN'; # (this happens if the strand file did not have entries for everything in raw manifest)
-            
-            my $manifestSummarySNP = $vals[2];
-            $manifestSummarySNP =~ /\[([A-Za-z]+)\/([A-Za-z]+)/;
-            my $firstRefAllele  = $1;
-            my $secondRefAllele = $2;
-            
-            $manifest{ $vals[0] } = [$vals[1], $firstRefAllele, $secondRefAllele, $vals[3], $vals[4], $vals[5]];
-        }
-        close($mfh);
-        
-        # go through the fcr file and output the VCF records
-        my $ifh = $fcr_file->openr;
-        $vcf_file->disconnect;
-        my $records = 0;
-        while (<$ifh>) {
-            chomp;
-            my @vals = split(/\t/);
-            next unless $vals[1] eq $library || $vals[1] eq $sample;
-            
-            my $genotypeSNPName   = $vals[0];
-            my $genotypeAlleleOne = $vals[2];
-            my $genotypeAlleleTwo = $vals[3];
-            my $genotypeGCScore   = $vals[4];
-            
-            # skip non-SNPs
-            if (($genotypeAlleleOne eq 'D') || ($genotypeAlleleOne eq 'I')) {
-                next;
-            }
-            if (($genotypeAlleleOne eq '-') || ($genotypeAlleleTwo eq '-')) {
-                next;
-            }
-            
-            my $intensityA = $vals[7];
-            my $intensityB = $vals[8];
-            my $BAF        = $vals[11];
-            my $LRR        = $vals[12];
-            
-            # get the snp meta info from the manifest hash
-            my ($manifestSummaryIlmnStrand, $firstRefAllele, $secondRefAllele, $manifestSummaryChr, $manifestSummaryMapInfo, $manifestSummarySourceStrand) = @{ $manifest{$genotypeSNPName} || next };
-            
-            # flip to the ILMN strand if necessary (ie. 'BOT' in manifest)
-            if ($manifestSummaryIlmnStrand eq 'BOT') {
-                # flip to complements
-                $firstRefAllele =~ tr/ATGCatgc/TACGtacg/;
-                $secondRefAllele =~ tr/ATGCatgc/TACGtacg/;
-            }
-            
-            # match the alleles against the ref alleles
-            my $matchOne = my $matchTwo = 0;
-            if ($genotypeAlleleOne ne $firstRefAllele) {
-                $matchOne = 1;
-            }
-            if ($genotypeAlleleTwo ne $firstRefAllele) {
-                $matchTwo = 1;
-            }
-            
-            # flip to the forward strand if necessary (ie. '-' in manifest)
-            if ($manifestSummarySourceStrand eq '-') {
-                $firstRefAllele =~ tr/ATGCatgc/TACGtacg/;
-                $secondRefAllele =~ tr/ATGCatgc/TACGtacg/;
-            }
-            
-            # write conversion
-            print $ofh "$manifestSummaryChr\t$manifestSummaryMapInfo\t$genotypeSNPName\t$firstRefAllele\t$secondRefAllele\t.\t.\tNS=1\tGT:GC:IA:IB:BAF:LRR\t$matchOne/$matchTwo:$genotypeGCScore:$intensityA:$intensityB:$BAF:$LRR\n";
-            $records++;
-        }
-        $fcr_file->close;
-        $vcf_file_unsorted->close;
-        
-        # sort the vcf file
-        my $vcf_sort_cmd = "$vcf_sort " . $vcf_file_unsorted->path . " | $bgzip -c > " . $vcf_file->path;
-        system($vcf_sort_cmd) && die "VCF sort [$vcf_sort_cmd] failed\n";
-        
-        # check it has the correct number of lines
-        $vcf_file->update_stats_from_disc;
-        my $actual_records = $vcf_file->num_records;
-        unless ($actual_records == $records) {
-            die "Expected $records records in the sorted VCF file, but only got $actual_records\n";
-        }
     }
 }
 
