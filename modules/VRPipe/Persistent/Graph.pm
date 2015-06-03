9d99d5e8caf0cedfa125ff5b8f7cb64ea86f317c
Sendu Bala <sb10@sanger.ac.uk>
2015-06-03 10:11:39 +0100

    node_set_properties() now has more efficient cypher

--- modules/VRPipe/Persistent/Graph.pm
+++ modules/VRPipe/Persistent/Graph.pm
@@ -695,8 +695,7 @@ class VRPipe::Persistent::Graph {
     
     method node_set_properties (HashRef|Object $node!, HashRef $properties!) {
         my $id = $self->node_id($node);
-        my $properties_map = $self->_param_map($properties, 'param');
-        my ($updated_node) = @{ $self->_run_cypher([["MATCH (n) WHERE id(n) = $id SET n = $properties_map return n", { 'param' => $properties }]])->{nodes} };
+        my ($updated_node) = @{ $self->_run_cypher([["MATCH (n) WHERE id(n) = $id SET n = { props } return n", { 'props' => $properties }]])->{nodes} };
         $node->{properties} = $updated_node->{properties};
         return;
     }
