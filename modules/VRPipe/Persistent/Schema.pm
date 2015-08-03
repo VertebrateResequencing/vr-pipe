2112d3f509ee95a51e09c55ec5a5e045825882e2
Sendu Bala <sb10@sanger.ac.uk>
2015-08-03 09:51:55 +0100

    roll back version since 40 was never in master

--- modules/VRPipe/Persistent/Schema.pm
+++ modules/VRPipe/Persistent/Schema.pm
@@ -60,7 +60,7 @@ class VRPipe::Persistent::Schema extends VRPipe::Persistent::SchemaBase {
     use VRPipe::Persistent::SchemaBase;
     use VRPipe::Persistent::ConverterFactory;
     
-    our $VERSION = 41;
+    our $VERSION = 40;
     __PACKAGE__->load_classes({
             'VRPipe' => [
                 qw/Step Scheduler Job Requirements
