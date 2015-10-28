e401bc48354843a1990677ac27b989c6a5fa22ae
Yasin Memari <ym3@sanger.ac.uk>
2015-10-28 15:40:24 +0000

    new bam merge and mark duplicates pipeline that also fixes the @RG header lines

---
+++ modules/VRPipe/Pipelines/bam_merge_and_streaming_mark_duplicates_with_add_readgroup.pm
@@ -0,0 +1,68 @@
+
+=head1 NAME
+
+VRPipe::Pipelines::bam_merge_and_streaming_mark_duplicates_with_add_readgroup -
+a pipeline
+
+=head1 DESCRIPTION
+
+*** more documentation to come
+
+=head1 AUTHOR
+
+Yasin Memari <ym3@sanger.ac.uk>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2015 Genome Research Limited.
+
+This file is part of VRPipe.
+
+VRPipe is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see L<http://www.gnu.org/licenses/>.
+
+=cut
+
+use VRPipe::Base;
+
+class VRPipe::Pipelines::bam_merge_and_streaming_mark_duplicates_with_add_readgroup with VRPipe::PipelineRole {
+    method name {
+        return 'bam_merge_and_streaming_mark_duplicates_with_add_readgroup';
+    }
+    
+    method description {
+        return 'Merge and mark duplicates pipeline that also replaces the ^@RG lines in the bam headers using information in the metadata. Input files must have the lane metadata and the tags required by bamstreamingmarkduplicates.';
+    }
+    
+    method step_names {
+        (
+            'samtools_add_readgroup',                       #1
+            'samtools_merge_and_streaming_mark_duplicates', #2
+        );
+    }
+    
+    method adaptor_definitions {
+        (
+            { from_step => 0, to_step => 1, to_key   => 'bam_files' },
+            { from_step => 1, to_step => 2, from_key => 'rg_added_bam_files', to_key => 'bam_files' },
+        );
+    }
+    
+    method behaviour_definitions {
+        (
+            { after_step => 2, behaviour => 'delete_inputs',  act_on_steps => [0], regulated_by => 'delete_input_bams', default_regulation => 0 },
+            { after_step => 2, behaviour => 'delete_outputs', act_on_steps => [1], regulated_by => 'cleanup',           default_regulation => 1 },
+        );
+    }
+}
+
+1;
