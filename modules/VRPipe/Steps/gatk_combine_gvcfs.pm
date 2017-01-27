786aff69e06d4fcb6b5bcafe9a1f2b132e0f79b5
Yasin Memari <ym3@sanger.ac.uk>
2017-01-27 12:27:57 +0000

    restore the function of $maximum_gvcfs_to_combine which was lost in a previous commit

--- modules/VRPipe/Steps/gatk_combine_gvcfs.pm
+++ modules/VRPipe/Steps/gatk_combine_gvcfs.pm
@@ -120,16 +120,23 @@ class VRPipe::Steps::gatk_combine_gvcfs extends VRPipe::Steps::gatk_v2 {
                     $this_basename = "$region.$basename";
                     $this_basename =~ s/:/_/;
                 }
-                my @file_inputs = map { "--variant " . $_->path } @$region_input_files;
-                $this_combine_gvcfs_opts .= " @file_inputs";
                 
-                my $vcf_file  = $self->output_file(output_key => 'combine_gvcf_files', basename => $this_basename, type => 'vcf', metadata => $vcf_meta);
-                my $vcf_path  = $vcf_file->path;
-                my $vcf_index = $self->output_file(output_key => 'combine_gvcf_index_files', basename => $this_basename . ".tbi", type => 'bin', metadata => $vcf_meta);
-                
-                my $cmd      = $self->gatk_prefix($req->memory) . qq[ -T CombineGVCFs -R $reference_fasta $this_combine_gvcfs_opts -o $vcf_path];
-                my $this_cmd = "use VRPipe::Steps::gatk_combine_gvcfs; VRPipe::Steps::gatk_combine_gvcfs->combine_and_check(q[$cmd]);";
-                $self->dispatch_vrpipecode($this_cmd, $req, { output_files => [$vcf_file, $vcf_index] });
+                my $count = 0;
+                while (@$region_input_files) {
+                    my @files_list = splice @$region_input_files, 0, $maximum_gvcfs_to_combine;
+                    my @file_inputs = map { "--variant " . $_->path } @files_list;
+                    
+                    $this_combine_gvcfs_opts .= " @file_inputs";
+                    
+                    my $vcf_file  = $self->output_file(output_key => 'combine_gvcf_files', basename => "batch_$count." . $this_basename, type => 'vcf', metadata => $vcf_meta);
+                    my $vcf_path  = $vcf_file->path;
+                    my $vcf_index = $self->output_file(output_key => 'combine_gvcf_index_files', basename => "batch_$count." . $this_basename . ".tbi", type => 'bin', metadata => $vcf_meta);
+                    
+                    my $cmd      = $self->gatk_prefix($req->memory) . qq[ -T CombineGVCFs -R $reference_fasta $this_combine_gvcfs_opts -o $vcf_path];
+                    my $this_cmd = "use VRPipe::Steps::gatk_combine_gvcfs; VRPipe::Steps::gatk_combine_gvcfs->combine_and_check(q[$cmd]);";
+                    $self->dispatch_vrpipecode($this_cmd, $req, { output_files => [$vcf_file, $vcf_index] });
+                    $count++;
+                }
             }
         };
     }
